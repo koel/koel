a4335bff52f9322a652f227dae2d05ddea771d58
An Phan <an@construct.sg>
2015-12-13 14:29:32 +0800

    Fix publicPath in to allow php artisan serve

--- app/Application.php
+++ app/Application.php
@@ -11,6 +11,16 @@ use InvalidArgumentException;
 class Application extends IlluminateApplication
 {
     /**
+     * We have merged public path and base path.
+     *
+     * @return string
+     */
+    public function publicPath()
+    {
+        return $this->basePath;
+    }
+
+    /**
      * Loads a revision'ed asset file, making use of gulp-rev
      * This is a copycat of L5's Elixir, but catered to our directory structure.
      *
@@ -23,7 +33,7 @@ class Application extends IlluminateApplication
         static $manifest = null;
 
         if (is_null($manifest)) {
-            $manifest = json_decode(file_get_contents($this->publicPath().'/build/rev-manifest.json'), true);
+            $manifest = json_decode(file_get_contents($this->publicPath().'/public/build/rev-manifest.json'), true);
         }
 
         if (isset($manifest[$file])) {
