6a384f2f1cf4d570b66e0b1523525549b90ff60a
Leandro Henrique Reis <emtudo@gmail.com>
2015-12-14 21:54:53 -0200

    fixed title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Player - Emtudo</title>
+        <title>Koel</title>
         <meta name="description" content="{{ config('app.tagline') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
