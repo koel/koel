b3869d6544ecc0424556f0c313819140e821515d
Robin IT <info@robin-it.com>
2015-12-30 13:25:19 +0100

    Make nitpick-ci happy.

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -32,5 +32,6 @@ return [
         Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
         Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
         Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
-    ]
+    ],
+
 ];
