0a4e76721b74e1015149019ed5758c3e25cbd697
James Clark <s1127853@sms.ed.ac.uk>
2016-02-28 03:57:06 +0000

    Add track to songs table
    This allows a song to have the track number attribute

---
+++ database/migrations/2016_02_28_035052_add_track_to_songs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTrackToSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->integer('track')->after('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->dropColumn('track');
+        });
+    }
+}
