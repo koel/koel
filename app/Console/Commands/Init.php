b108af5d7c30b4fabd09bf519bd708b84d02d74f
Edvin Hultberg <hultberg@users.noreply.github.com>
2016-03-13 13:29:23 +0100

    Removed metion of bower in koel:init command

--- app/Console/Commands/Init.php
+++ app/Console/Commands/Init.php
@@ -67,7 +67,7 @@ class Init extends Command
             $this->comment('Data seeded -- skipping');
         }
 
-        $this->info('Executing npm install, bower install, gulp and whatnot');
+        $this->info('Executing npm install, gulp and whatnot');
         system('npm install');
 
         $this->comment("\n🎆  Success! You can now run Koel from localhost with `php artisan serve`.");
