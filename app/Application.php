f4e5a4501db98956c90a9ae045e82772b75706a0
An Phan <an@construct.sg>
2016-03-28 20:22:18 +0800

    Bump version

--- app/Application.php
+++ app/Application.php
@@ -18,7 +18,7 @@ class Application extends IlluminateApplication
      *
      * @link https://github.com/phanan/koel/releases
      */
-    const VERSION = 'v2.2.0';
+    const VERSION = 'v2.3.0';
 
     /**
      * We have merged public path and base path.
