e5f77399fd1058fb9dd869ae0a088909723a1c87
An Phan <an@construct.sg>
2016-05-27 16:45:45 +0800

    Add song scrobble test

--- tests/LastfmTest.php
+++ tests/LastfmTest.php
@@ -203,4 +203,21 @@ class LastfmTest extends TestCase
 
         (new UpdateLastfmNowPlaying($lastfm))->handle(new SongStartedPlaying($song, $user));
     }
+
+    public function testScrobble()
+    {
+        $this->withoutEvents();
+        $this->createSampleMediaSet();
+
+        $user = factory(User::class)->create(['preferences' => ['lastfm_session_key' => 'bar']]);
+        $song = Song::first();
+
+        $ts = time();
+
+        $lastfm = m::mock(Lastfm::class, ['enabled' => true]);
+        $lastfm->shouldReceive('scrobble')
+            ->with($song->album->artist->name, $song->title, $ts, $song->album->name, 'bar');
+
+        $this->post("/api/{$song->id}/scrobble/$ts");
+    }
 }
