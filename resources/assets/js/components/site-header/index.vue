682020174f94c65aa2824c95ad1cd6e64cde2638
An Phan <me@phanan.net>
2016-06-26 19:01:07 +0800

    Fix app title display issue

--- resources/assets/js/components/site-header/index.vue
+++ resources/assets/js/components/site-header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <header id="mainHeader">
-    <h1 class="brand" v-once="appTitle"></h1>
+    <h1 class="brand">{{ appTitle }}</h1>
     <span class="hamburger" @click="toggleSidebar">
       <i class="fa fa-bars"></i>
     </span>
