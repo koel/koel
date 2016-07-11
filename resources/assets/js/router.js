8cd9e66485274455d76021f81b68e12ee657271a
An Phan <me@phanan.net>
2016-07-11 23:03:48 +0800

    Fix song sharing on iDevice (for real)

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -74,7 +74,7 @@ export default {
       if (isMobile.apple.device) {
         // Mobile Safari doesn't allow autoplay, so we just queue.
         queueStore.queue(song);
-        this.parent.go('queue');
+        loadMainView('queue');
       } else {
         playback.queueAndPlay(song);
       }
@@ -82,7 +82,6 @@ export default {
   },
 
   init() {
-    this.routes.parent = this;
     this.loadState();
     window.addEventListener('popstate', () => this.loadState(), true);
   },
