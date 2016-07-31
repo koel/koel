93982aa9a7f84ea9b83a807a515abaaac1a57210
An Phan <me@phanan.net>
2016-07-31 11:36:20 +0800

    Bump version

--- app/Application.php
+++ app/Application.php
@@ -19,7 +19,7 @@ class Application extends IlluminateApplication
      *
      * @link https://github.com/phanan/koel/releases
      */
-    const VERSION = 'v3.1.1';
+    const VERSION = 'v3.2.0';
 
     /**
      * We have merged public path and base path.
