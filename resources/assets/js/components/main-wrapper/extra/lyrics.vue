a60f15ca3c41a332e4b1500b858ebca0d7860a3c
An Phan <me@phanan.net>
2016-11-28 11:32:09 +0700

    Hide lyrics hint if no song

--- resources/assets/js/components/main-wrapper/extra/lyrics.vue
+++ resources/assets/js/components/main-wrapper/extra/lyrics.vue
@@ -2,7 +2,7 @@
   <article id="lyrics">
     <div class="content">
       <div v-if="song.lyrics" v-html="song.lyrics"/>
-      <p class="none" v-else>No lyrics found. Are you not listening to Bach?</p>
+      <p class="none" v-if="song.id && !song.lyrics">No lyrics found. Are you not listening to Bach?</p>
     </div>
   </article>
 </template>
