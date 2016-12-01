43a031da79ec54c7163bf472955aeb59ce417610
An Phan <me@phanan.net>
2016-12-01 20:58:39 +0700

    Merge master

--- resources/assets/js/components/main-wrapper/sidebar/index.vue
+++ resources/assets/js/components/main-wrapper/sidebar/index.vue
@@ -155,145 +155,6 @@ export default {
 </script>
 
 <style lang="sass">
-<<<<<<< HEAD
-    @import "../../../../sass/partials/_vars.scss";
-    @import "../../../../sass/partials/_mixins.scss";
-
-    #sidebar {
-        flex: 0 0 256px;
-        background-color: $colorSidebarBgr;
-        padding: 22px 0 0;
-        max-height: calc(100vh - #{$headerHeight + $footerHeight});
-        overflow: auto;
-        -ms-overflow-style: -ms-autohiding-scrollbar;
-
-        html.touchevents & {
-            // Enable scroll with momentum on touch devices
-            overflow-y: scroll;
-            -webkit-overflow-scrolling: touch;
-        }
-
-        a.droppable {
-            transform: scale(1.2);
-            transition: .3s;
-            transform-origin: center left;
-
-            color: $colorMainText;
-            background-color: rgba(0, 0, 0, .3);
-        }
-
-        section {
-            margin-bottom: 32px;
-
-            h1 {
-                text-transform: uppercase;
-                letter-spacing: 1px;
-                padding: 0 16px;
-                margin-bottom: 12px;
-
-                i {
-                    float: right;
-                }
-            }
-
-            a {
-                display: block;
-                height: 36px;
-                line-height: 36px;
-                padding: 0 12px 0 16px;
-                border-left: 4px solid transparent;
-
-                &.active, &:hover {
-                    border-left-color: $colorHighlight;
-                    color: $colorLinkHovered;
-                    background: rgba(255, 255, 255, .05);
-                }
-
-                &:hover {
-                    border-left-color: darken($colorHighlight, 20%);
-                }
-
-                &::before {
-                    width: 24px;
-                    display: inline-block;
-                    font-family: FontAwesome;
-                }
-
-                &.home::before {
-                    content: "\f015";
-                }
-
-                &.queue::before {
-                    content: "\f0cb";
-                }
-
-                &.songs::before {
-                    content: "\f001";
-                }
-
-                &.albums::before {
-                    content: "\f152";
-                }
-
-                &.artists::before {
-                    content: "\f130";
-                }
-
-                &.settings::before {
-                    content: "\f013";
-                }
-
-                &.users::before {
-                    content: "\f0c0";
-                }
-            }
-        }
-
-        .new-ver, .credits {
-            margin: 16px;
-            padding: 16px;
-            border: 1px solid $color2ndText;
-            color: $colorMainText;
-            opacity: .3;
-            font-size: 90%;
-            display: block;
-            transition: .3s;
-
-            &:hover {
-                opacity: .7;
-            }
-        }
-
-        .credits {
-            border-color: #9cd121;
-            padding: 8px;
-            opacity: .7;
-
-            &:hover {
-                opacity: 1;
-            }
-
-            a {
-                color: #9cd121;
-            }
-        }
-
-
-        @media only screen and (max-width : 667px) {
-            position: fixed;
-            height: calc(100vh - #{$headerHeight + $footerHeight});
-            padding-bottom: $footerHeight; // make sure the footer can never overlap the content
-            width: 100%;
-            z-index: 99;
-            top: $headerHeight;
-            left: -100%;
-            transition: left .3s ease-in;
-
-            &.showing {
-                left: 0;
-            }
-        }
-=======
 @import "../../../../sass/partials/_vars.scss";
 @import "../../../../sass/partials/_mixins.scss";
 
@@ -392,21 +253,34 @@ export default {
     }
   }
 
-  .new-ver {
+  .new-ver, .credits {
     margin: 16px;
     padding: 16px;
     border: 1px solid $color2ndText;
     color: $colorMainText;
     opacity: .3;
-    font-size: .9rem;
+    font-size: 90%;
     display: block;
     transition: .3s;
 
     &:hover {
-      opacity: .7;
+        opacity: .7;
     }
   }
 
+  .credits {
+    border-color: #9cd121;
+    padding: 8px;
+    opacity: .7;
+
+    &:hover {
+        opacity: 1;
+    }
+
+    a {
+        color: #9cd121;
+    }
+  }
 
   @media only screen and (max-width : 667px) {
     position: fixed;
@@ -420,7 +294,6 @@ export default {
 
     &.showing {
       left: 0;
->>>>>>> master
     }
   }
 }
