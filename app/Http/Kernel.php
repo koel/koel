1a7197b33b8eaad2fdd8e0c3bd4da2727d8f9f2c
Phan An <me@phanan.net>
2017-02-15 03:19:19 +0000

    Apply fixes from StyleCI

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,10 +10,10 @@ use App\Http\Middleware\UseDifferentConfigIfE2E;
 use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 class Kernel extends HttpKernel
 {
--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -7,10 +7,10 @@ use Illuminate\Foundation\Http\Middleware\TrimStrings as BaseTrimmer;
 class TrimStrings extends BaseTrimmer
 {
     /**
-    * The names of the attributes that should not be trimmed.
-    *
-    * @var array
-    */
+     * The names of the attributes that should not be trimmed.
+     *
+     * @var array
+     */
     protected $except = [
         'password',
         'password_confirmation',
