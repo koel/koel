59a59deb40e3cc56ffe6d9fc0af74f928e555a92
Phan An <me@phanan.net>
2017-04-22 23:42:45 +0800

    Fix wrong config path

--- resources/assets/js/config/index.js
+++ resources/assets/js/config/index.js
@@ -1,4 +1,4 @@
 export default {
-  unknownCover: (typeof window !== 'undefined' ? window.location.href : '/') + 'public/img/covers/unknown-album.png',
+  unknownCover: (typeof window !== 'undefined' ? window.location.href.replace(window.location.hash, '') : '/') + 'public/img/covers/unknown-album.png',
   appTitle: 'Koel'
 }
