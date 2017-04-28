328dc88c9960b9986a7a638ea20a1881641f9a06
Phan An <me@phanan.net>
2017-04-28 22:59:13 +0800

    Apply fixes from StyleCI (#583)

--- tests/Feature/MediaCacheTest.php
+++ tests/Feature/MediaCacheTest.php
@@ -2,20 +2,20 @@
 
 namespace Tests\Feature;
 
-use MediaCache;
 use Cache;
+use MediaCache;
 use Tests\BrowserKitTestCase;
 
 class MediaCacheTest extends BrowserKitTestCase
 {
-	public function testGetCache()
-	{
-		// first clear all cache
-		Cache::flush();
-		// now make sure the get() function returns data
-		$data = MediaCache::get();
-		$this->assertNotNull($data);
-		// and the data are seen in the cache as well
-		$this->assertEquals($data, Cache::get('media_cache'));
-	}
+    public function testGetCache()
+    {
+        // first clear all cache
+        Cache::flush();
+        // now make sure the get() function returns data
+        $data = MediaCache::get();
+        $this->assertNotNull($data);
+        // and the data are seen in the cache as well
+        $this->assertEquals($data, Cache::get('media_cache'));
+    }
 }
