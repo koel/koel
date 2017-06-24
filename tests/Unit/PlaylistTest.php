d66522bd377eaf7e1bab9678f5187a269613399a
Phan An <me@phanan.net>
2017-06-24 21:48:54 +0100

    Finish Playlist unit test

---
+++ tests/Unit/PlaylistTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Playlist;
+use Tests\TestCase;
+
+class PlaylistTest extends TestCase
+{
+    /** @test */
+    public function it_can_be_instantiated()
+    {
+        $this->assertInstanceOf(Playlist::class, new Playlist());
+    }
+}
