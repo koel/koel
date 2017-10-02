d9d14b43a49b06e8fc084ad67adc824bcc0c5358
Phan An <me@phanan.net>
2017-10-02 18:51:13 +0100

    Make remote app work agaiin

--- resources/assets/js/remote/app.js
+++ resources/assets/js/remote/app.js
@@ -1,10 +1,11 @@
 import './static-loader'
 import Vue from 'vue'
 import { http } from '../services'
+import App from './app.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(require('./app.vue')),
+  render: h => h(App),
   created () {
     http.init()
   }
