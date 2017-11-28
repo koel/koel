20ab22b18e0d3be8267b48fac66037b034de1920
Fiete Börner <fieteboerner@googlemail.com>
2017-11-28 22:23:23 +0100

    add disc field to songs table via migration

---
+++ database/migrations/2017_11_27_184010_add_disc_into_songs.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDiscIntoSongs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->integer('disc')->after('track')->default(1)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->dropColumn('disc');
+        });
+    }
+}
