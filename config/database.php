baa5b7af13e7f66ff1d2df1778c65757a73e478f
leowmjw <leowmjw@users.noreply.github.com>
2017-12-03 18:40:35 +0800

    Add ability to customize port DB (#655)
    * Summary: Add the ability to configure DB ports
    
    * Summary: Reorder port
    
    * Summary: Added example env for DB_PORT customization
    
    Changesets:
    - Added DB_PORT and explain about customization

--- config/database.php
+++ config/database.php
@@ -48,6 +48,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', 3306),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
@@ -60,6 +61,7 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', 5432),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
@@ -71,6 +73,7 @@ return [
         'sqlsrv' => [
             'driver'   => 'sqlsrv',
             'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', 1433),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
