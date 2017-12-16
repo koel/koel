fa3a8e0c787f6d4776b80a7df14f1484c46adfb2
Phan An <me@phanan.net>
2017-12-17 00:13:03 +0100

    Add tests for songs.vue

---
+++ resources/assets/js/tests/components/main-wrapper/songs.spec.js
@@ -0,0 +1,13 @@
+import Component from '@/components/main-wrapper/main-content/songs.vue'
+import SongList from '@/components/shared/song-list.vue'
+import factory from '@/tests/factory'
+import { songStore } from '@/stores'
+
+describe('components/main-wrapper/main-content/settings', () => {
+  it('renders properly', () => {
+    songStore.all = factory('song', 10)
+    const wrapper = shallow(Component)
+    wrapper.find('h1.heading').text().should.contain('All Songs')
+    wrapper.contains(SongList).should.be.true
+  })
+})
