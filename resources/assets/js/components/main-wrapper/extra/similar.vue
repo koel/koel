ad056c2743dc244f2514b5dda89abdd64ee303f3
matiasdelellis <mati86dl@gmail.com>
2018-02-02 11:08:58 -0300

    Add panel to show a list of similar songs

---
+++ resources/assets/js/components/main-wrapper/extra/similar.vue
@@ -0,0 +1,34 @@
+<template>
+  <article id="similar">
+    <div class="content">
+      <section class="track-listing" v-if="song.similar.tracks.length">
+        <h1>Similar Tracks</h1>
+        <ul class="tracks">
+          <li v-for="track in song.similar.tracks">
+            {{ track.title }} - {{ track.artist }}
+          </li>
+         </ul>
+        </section>
+      <p class="none" v-if="song.id && !song.lyrics">No similar tracks found.</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    song: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~#/partials/_vars.scss";
+@import "~#/partials/_mixins.scss";
+#similar {
+  @include artist-album-info();
+}
+</style>
