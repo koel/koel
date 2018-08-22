c87ef99c1a522a7ced9abfd5f651a5fa4abb673e
Phan An <me@phanan.net>
2018-08-22 20:25:15 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/API/Download/FavoritesController.php
+++ app/Http/Controllers/API/Download/FavoritesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API\Download;
 
 use App\Http\Requests\API\Download\Request;
-use App\Models\Song;
 use App\Services\DownloadService;
 use App\Services\InteractionService;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
