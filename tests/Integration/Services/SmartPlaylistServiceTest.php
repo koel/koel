542c763924ca9f2c5be66e6eda4ee58e6b81ac2b
Phan An <me@phanan.net>
2018-11-03 22:56:26 +0100

    Add "Smart Playlist" backend logics

--- tests/Integration/Services/SmartPlaylistServiceTest.php
+++ tests/Integration/Services/SmartPlaylistServiceTest.php
@@ -210,7 +210,7 @@ class SmartPlaylistServiceTest extends TestCase
      * @dataProvider provideRules
      *
      * @param string[] $rules
-     * @param mixed[]  $bindings
+     * @param mixed[] $bindings
      */
     public function testBuildQueryForRules(array $rules, string $sql, array $bindings): void
     {
@@ -218,7 +218,7 @@ class SmartPlaylistServiceTest extends TestCase
         $this->assertSame($sql, $query->toSql());
         $queryBinding = $query->getBindings();
 
-        for ($i = 0, $count = count($queryBinding); $i < $count; $i++) {
+        for ($i = 0, $count = count($queryBinding); $i < $count; ++$i) {
             $this->assertSame(
                 $bindings[$i],
                 is_object($queryBinding[$i]) ? (string) $queryBinding[$i] : $queryBinding[$i]
@@ -328,11 +328,9 @@ class SmartPlaylistServiceTest extends TestCase
 
     public function testRulesWithUser(): void
     {
-        /**
-         * @var User
-         * @var User $alice
-         */
+        /** @var User $bob */
         $bob = factory(User::class)->create();
+        /** @var User $alice */
         $alice = factory(User::class)->create();
 
         $bobSong = factory(Song::class)->create([
