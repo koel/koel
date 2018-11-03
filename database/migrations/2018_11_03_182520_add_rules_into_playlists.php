8b338f7cef75c4c8aef299fc5b21fc68d992379d
Phan An <me@phanan.net>
2018-11-03 23:15:51 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_11_03_182520_add_rules_into_playlists.php
+++ database/migrations/2018_11_03_182520_add_rules_into_playlists.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Models\Playlist;
-use App\Models\User;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddRulesIntoPlaylists extends Migration
 {
