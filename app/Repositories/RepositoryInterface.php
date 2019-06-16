22e946076cfb83c186371a52cbc8750d4298a957
Phan An <me@phanan.net>
2019-06-16 23:14:37 +0200

    Fix return type

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -2,8 +2,8 @@
 
 namespace App\Repositories;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 
 interface RepositoryInterface
 {
