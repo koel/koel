a19e30e74a68b4dfa3512a4763fa659db995dd4a
Phan An <me@phanan.net>
2019-06-30 17:26:27 +0200

    Use constants for HTTP status code

--- app/Services/Streamers/PHPStreamer.php
+++ app/Services/Streamers/PHPStreamer.php
@@ -3,7 +3,6 @@
 namespace App\Services\Streamers;
 
 use DaveRandom\Resume\FileResource;
-use function DaveRandom\Resume\get_request_header;
 use DaveRandom\Resume\InvalidRangeHeaderException;
 use DaveRandom\Resume\NonExistentFileException;
 use DaveRandom\Resume\RangeSet;
@@ -11,6 +10,8 @@ use DaveRandom\Resume\ResourceServlet;
 use DaveRandom\Resume\SendFileFailureException;
 use DaveRandom\Resume\UnreadableFileException;
 use DaveRandom\Resume\UnsatisfiableRangeException;
+use Symfony\Component\HttpFoundation\Response;
+use function DaveRandom\Resume\get_request_header;
 
 class PHPStreamer extends Streamer implements DirectStreamerInterface
 {
@@ -18,18 +19,18 @@ class PHPStreamer extends Streamer implements DirectStreamerInterface
     {
         try {
             $rangeSet = RangeSet::createFromHeader(get_request_header('Range'));
-            $resource = new FileResource($this->song->path, 'application/octet-stream');
+            $resource = new FileResource($this->song->path);
             (new ResourceServlet($resource))->sendResource($rangeSet);
         } catch (InvalidRangeHeaderException $e) {
-            abort(400);
+            abort(Response::HTTP_BAD_REQUEST);
         } catch (UnsatisfiableRangeException $e) {
-            abort(416);
+            abort(Response::HTTP_REQUESTED_RANGE_NOT_SATISFIABLE);
         } catch (NonExistentFileException $e) {
-            abort(404);
+            abort(Response::HTTP_NOT_FOUND);
         } catch (UnreadableFileException $e) {
-            abort(500);
+            abort(Response::HTTP_INTERNAL_SERVER_ERROR);
         } catch (SendFileFailureException $e) {
-            abort_unless(headers_sent(), 500);
+            abort_unless(headers_sent(), Response::HTTP_INTERNAL_SERVER_ERROR);
             echo "An error occurred while attempting to send the requested resource: {$e->getMessage()}";
         }
 
