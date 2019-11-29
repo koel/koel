3e245ee13e5d8e0efa34aaf31f2b9c6e824beedc
Phan An <me@phanan.net>
2019-11-29 21:51:39 +0100

    Apply fixes from StyleCI (#1128)

--- database/seeds/ArtistTableSeeder.php
+++ database/seeds/ArtistTableSeeder.php
@@ -8,7 +8,7 @@ class ArtistTableSeeder extends Seeder
     public function run()
     {
         Artist::firstOrCreate([
-            'id' => Artist::UNKNOWN_ID
+            'id' => Artist::UNKNOWN_ID,
         ], [
             'id' => Artist::UNKNOWN_ID,
             'name' => Artist::UNKNOWN_NAME,
