54329d89b018edf9f5a4951cedec6f666d24803a
Phan An <me@phanan.net>
2019-11-29 20:51:32 +0000

    Apply fixes from StyleCI

--- database/seeds/AlbumTableSeeder.php
+++ database/seeds/AlbumTableSeeder.php
@@ -9,7 +9,7 @@ class AlbumTableSeeder extends Seeder
     public function run()
     {
         Album::firstOrCreate([
-            'id' => Album::UNKNOWN_ID
+            'id' => Album::UNKNOWN_ID,
         ], [
             'id' => Album::UNKNOWN_ID,
             'artist_id' => Artist::UNKNOWN_ID,
