40d4671b28c18a5451e068b6c3088243f49141d2
Phan An <me@phanan.net>
2020-06-10 19:55:17 +0200

    chore: bump version

--- app/Application.php
+++ app/Application.php
@@ -15,7 +15,7 @@ class Application extends IlluminateApplication
      *
      * @link https://github.com/phanan/koel/releases
      */
-    public const KOEL_VERSION = 'v4.3.1';
+    public const KOEL_VERSION = 'v4.4.0';
 
     /**
      * We have merged public path and base path.
