07cd8bab9e0b1471a2fcab94ec45a69a16a49d56
Saeed Rostami <privatemx.2010@gmail.com>
2020-12-13 16:08:49 +0330

    Create passwords.php

---
+++ resources/lang/fa/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset'     => 'رمز عبور شما بازگردانی شد!',
+    'sent'      => 'لینک بازگردانی رمز عبور به ایمیل شما ارسال شد.',
+    'throttled' => 'پیش از تلاش مجدد کمی صبر کنید.',
+    'token'     => 'مشخصه‌ی بازگردانی رمز عبور معتبر نیست.',
+    'user'      => 'ما کاربری با این نشانی ایمیل نداریم!',
+];
