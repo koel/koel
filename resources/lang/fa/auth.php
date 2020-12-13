75b2ab090bff37f376a8c84fff2b945c7939a88c
Saeed Rostami <privatemx.2010@gmail.com>
2020-12-13 16:06:28 +0330

    Create auth.php

---
+++ resources/lang/fa/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'مشخصات وارد شده با اطلاعات ما سازگار نیست.',
+    'throttle' => 'دفعات تلاش شما برای ورود بیش از حد مجاز است. لطفا پس از :seconds ثانیه مجددا تلاش فرمایید.',
+    'password' => 'رمز عبور شما معتبر نیست.',
+];
