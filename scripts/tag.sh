8e100a2a2e1646df329c2cbe33552ac09331b0fc
Phan An <me@phanan.net>
2021-05-05 14:39:35 +0200

    chore: add some script improvements

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ `git status --porcelain` ]]; then
+if [[ $(git status --porcelain) ]]; then
   echo 'ERROR: Please commit all changes before tagging a new version.'
   exit 1
 fi
@@ -13,16 +13,16 @@ fi
 cd ..
 
 TAG=$1
-echo $TAG > .version
-cd ./resources/assets
-git -c color.ui=always tag $TAG
+echo "$TAG" > .version
+cd ./resources/assets || exit
+git -c color.ui=always tag "$TAG"
 git -c color.ui=always push --tags
 
 cd ../..
 git -c color.ui=always add .
 git -c color.ui=always commit -m "chore(release): bump version to ${TAG}"
 git -c color.ui=always push
-git -c color.ui=always tag $TAG
+git -c color.ui=always tag "$TAG"
 git -c color.ui=always tag latest -f
 git -c color.ui=always push --tags -f
 
