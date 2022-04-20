b636a4519d424c40e13b01dc28941bda83bf116a
Phan An <me@phanan.net>
2022-04-20 13:51:40 +0200

    fix: focus directive

--- resources/assets/js/directives/focus.ts
+++ resources/assets/js/directives/focus.ts
@@ -4,5 +4,5 @@ import { Directive } from 'vue'
  * A simple directive to set focus into an input field when it's shown.
  */
 export const focus: Directive = {
-  created: (el: HTMLElement) => el.focus()
+  mounted: (el: HTMLElement) => el.focus()
 }
