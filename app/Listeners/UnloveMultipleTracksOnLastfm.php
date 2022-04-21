c4365ae88cfdc534c3cc78b612b6ece86e70c0cc
Phan An <me@phanan.net>
2022-04-21 18:36:11 +0200

    fix: batch unfavorite songs

--- app/Listeners/UnloveMultipleTracksOnLastfm.php
+++ app/Listeners/UnloveMultipleTracksOnLastfm.php
@@ -3,7 +3,9 @@
 namespace App\Listeners;
 
 use App\Events\SongsBatchUnliked;
+use App\Models\Song;
 use App\Services\LastfmService;
+use App\Values\LastfmLoveTrackParameters;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class UnloveMultipleTracksOnLastfm implements ShouldQueue
@@ -17,6 +19,12 @@ class UnloveMultipleTracksOnLastfm implements ShouldQueue
 
     public function handle(SongsBatchUnliked $event): void
     {
-        $this->lastfm->batchToggleLoveTracks($event->songs, $event->user, false);
+        $this->lastfm->batchToggleLoveTracks(
+            $event->songs->map(static function (Song $song): LastfmLoveTrackParameters {
+                return LastfmLoveTrackParameters::make($song->title, $song->artist->name);
+            }),
+            $event->user,
+            false
+        );
     }
 }
