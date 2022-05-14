2acd1c2317cd09b6de22d54ea0530b488d7a787d
Phan An <me@phanan.net>
2022-05-14 17:54:26 +0200

    chore: clean up alerts

--- resources/assets/js/utils/alerts.ts
+++ resources/assets/js/utils/alerts.ts
@@ -7,23 +7,20 @@ const encodeEntities = (str: string) => str.replace(/&/g, '&amp;')
   .replace(/>/g, '&gt;')
 
 export const alerts = {
-  alert: (msg: string) => alertify.alert(encodeEntities(msg)),
+  alert: (message: string) => alertify.alert(encodeEntities(message)),
+  confirm: (message: string, onOk: Closure, onCancel?: Closure) => alertify.confirm(message, onOk, onCancel),
 
-  confirm: (msg: string, okFunc: Closure, cancelFunc?: Closure) => {
-    alertify.confirm(msg, okFunc, cancelFunc)
-  },
-
-  log: (msg: string, type: LogType = 'log', cb?: Closure) => {
+  log: (message: string, type: LogType, callback?: Closure) => {
     alertify.logPosition('top right')
     alertify.closeLogOnClick(true)
-    alertify[type](encodeEntities(msg), cb)
+    alertify[type](encodeEntities(message), callback)
   },
 
-  success (msg: string, cb?: Closure) {
-    this.log(msg, 'success', cb)
+  success (message: string, callback?: Closure) {
+    this.log(message, 'success', callback)
   },
 
-  error (msg: string, cb?: Closure) {
-    this.log(msg, 'error', cb)
+  error (message: string, callback?: Closure) {
+    this.log(message, 'error', callback)
   }
 }
