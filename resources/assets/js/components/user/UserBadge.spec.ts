07ee6f59e8f3ecf1c795cff12f12a0d77a935727
Phan An <me@phanan.net>
2022-05-15 17:32:38 +0200

    feat(test): add UserBadge component tests

---
+++ resources/assets/js/components/user/UserBadge.spec.ts
@@ -0,0 +1,27 @@
+import UnitTestCase from '@/__tests__/UnitTestCase'
+import factory from '@/__tests__/factory'
+import { expect, it } from 'vitest'
+import { eventBus } from '@/utils'
+import { fireEvent } from '@testing-library/vue'
+import UserBadge from './UserBadge.vue'
+
+new class extends UnitTestCase {
+  private renderComponent () {
+    return this.actingAs(factory<User>('user', {
+      name: 'John Doe'
+    })).render(UserBadge)
+  }
+
+  protected test () {
+    it('renders', () => expect(this.renderComponent().html()).toMatchSnapshot())
+
+    it('logs out', async () => {
+      const mock = this.mock(eventBus, 'emit')
+      const { getByTestId } = this.renderComponent()
+
+      await fireEvent.click(getByTestId('btn-logout'))
+
+      expect(mock).toHaveBeenCalledOnce()
+    })
+  }
+}
