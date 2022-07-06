9d79d95cb9b4c35d78f61c700c5c9f8ad50e21d6
Phan An <me@phanan.net>
2022-07-06 13:05:21 +0200

    refactor: massively simplify SmartPlaylist logic

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use App\Events\LibraryChanged;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -48,12 +48,7 @@ class Song extends Model
     public $incrementing = false;
     protected $guarded = [];
 
-    /**
-     * Attributes to be hidden from JSON outputs.
-     * Here we specify to hide lyrics as well to save some bandwidth (actually, lots of it).
-     * Lyrics can then be queried on demand.
-     */
-    protected $hidden = ['lyrics', 'updated_at', 'path', 'mtime'];
+    protected $hidden = ['updated_at', 'path', 'mtime'];
 
     protected $casts = [
         'length' => 'float',
@@ -63,109 +58,6 @@ class Song extends Model
     ];
 
     protected $keyType = 'string';
-
-    /**
-     * Update song info.
-     *
-     * @param array<string> $ids
-     * @param array<string> $data the data array, with these supported fields:
-     * - title
-     * - artistName
-     * - albumName
-     * - lyrics
-     * All of these are optional, in which case the info will not be changed
-     * (except for lyrics, which will be emptied)
-     *
-     * @return Collection|array<Song>
-     */
-    public static function updateInfo(array $ids, array $data): Collection
-    {
-        /*
-         * A collection of the updated songs.
-         *
-         * @var Collection
-         */
-        $updatedSongs = collect();
-
-        $ids = (array) $ids;
-        // If we're updating only one song, take into account the title, lyrics, and track number.
-        $single = count($ids) === 1;
-
-        foreach ($ids as $id) {
-            /** @var Song|null $song */
-            $song = self::with('album', 'album.artist')->find($id);
-
-            if (!$song) {
-                continue;
-            }
-
-            $updatedSongs->push($song->updateSingle(
-                $single ? trim($data['title']) : $song->title,
-                trim($data['albumName'] ?: $song->album->name),
-                trim($data['artistName']) ?: $song->artist->name,
-                $single ? trim($data['lyrics']) : $song->lyrics,
-                $single ? (int) $data['track'] : $song->track,
-                (int) $data['compilationState']
-            ));
-        }
-
-        // Our library may have been changed. Broadcast an event to tidy it up if need be.
-        if ($updatedSongs->count()) {
-            event(new LibraryChanged());
-        }
-
-        return $updatedSongs;
-    }
-
-    public function updateSingle(
-        string $title,
-        string $albumName,
-        string $artistName,
-        string $lyrics,
-        int $track,
-        int $compilationState
-    ): self {
-        if ($artistName === Artist::VARIOUS_NAME) {
-            // If the artist name is "Various Artists", it's a compilation song no matter what.
-            $compilationState = 1;
-            // and since we can't determine the real contributing artist, it's "Unknown"
-            $artistName = Artist::UNKNOWN_NAME;
-        }
-
-        $artist = Artist::getOrCreate($artistName);
-
-        switch ($compilationState) {
-            case 1: // ALL, or forcing compilation status to be Yes
-                $isCompilation = true;
-                break;
-
-            case 2: // Keep current compilation status
-                $isCompilation = $this->album->artist_id === Artist::VARIOUS_ID;
-                break;
-
-            default:
-                $isCompilation = false;
-                break;
-        }
-
-        $album = Album::getOrCreate($artist, $albumName, $isCompilation);
-
-        $this->artist_id = $artist->id;
-        $this->album_id = $album->id;
-        $this->title = $title;
-        $this->lyrics = $lyrics;
-        $this->track = $track;
-
-        $this->save();
-
-        // Clean up unnecessary data from the object
-        unset($this->album);
-        unset($this->artist);
-        // and make sure the lyrics is shown
-        $this->makeVisible('lyrics');
-
-        return $this;
-    }
 
     public function artist(): BelongsTo
     {
@@ -198,22 +90,12 @@ class Song extends Model
         return $query->where('path', 'LIKE', "$path%");
     }
 
-    /**
-     * Sometimes the tags extracted from getID3 are HTML entity encoded.
-     * This makes sure they are always sane.
-     */
-    public function setTitleAttribute(string $value): void
+    protected function title(): Attribute
     {
-        $this->attributes['title'] = html_entity_decode($value);
-    }
-
-    /**
-     * Some songs don't have a title.
-     * Fall back to the file name (without extension) for such.
-     */
-    public function getTitleAttribute(?string $value): string
-    {
-        return $value ?: pathinfo($this->path, PATHINFO_FILENAME);
+        return new Attribute(
+            get: fn (?string $value) => $value ?: pathinfo($this->path, PATHINFO_FILENAME),
+            set: static fn (string $value) => html_entity_decode($value)
+        );
     }
 
     public static function withMeta(User $scopedUser, ?Builder $query = null): Builder
--- app/Services/SmartPlaylistService.php
+++ app/Services/SmartPlaylistService.php
@@ -3,119 +3,38 @@
 namespace App\Services;
 
 use App\Exceptions\NonSmartPlaylistException;
-use App\Factories\SmartPlaylistRuleParameterFactory;
 use App\Models\Playlist;
 use App\Models\Song;
-use App\Models\User;
 use App\Values\SmartPlaylistRule;
 use App\Values\SmartPlaylistRuleGroup;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 
 class SmartPlaylistService
 {
-    private const USER_REQUIRING_RULE_PREFIXES = ['interactions.'];
-
-    private SmartPlaylistRuleParameterFactory $parameterFactory;
-
-    public function __construct(SmartPlaylistRuleParameterFactory $parameterFactory)
+    public function __construct(private Guard $auth)
     {
-        $this->parameterFactory = $parameterFactory;
     }
 
-    /** @return Collection|array<Song> */
+    /** @return Collection|array<array-key, Song> */
     public function getSongs(Playlist $playlist): Collection
     {
         throw_unless($playlist->is_smart, NonSmartPlaylistException::create($playlist));
 
-        $ruleGroups = $this->addRequiresUserRules($playlist->rule_groups, $playlist->user);
+        $query = Song::withMeta($this->auth->user());
 
-        return $this->buildQueryFromRules($ruleGroups, $playlist->user)
-            ->orderBy('songs.title')
-            ->get();
-    }
+        $playlist->rule_groups->each(static function (SmartPlaylistRuleGroup $group, int $index) use ($query): void {
+            $clause = $index === 0 ? 'where' : 'orWhere';
 
-    public function buildQueryFromRules(Collection $ruleGroups, User $user): Builder
-    {
-        $query = Song::withMeta($user);
-
-        $ruleGroups->each(function (SmartPlaylistRuleGroup $group) use ($query): void {
-            $query->orWhere(function (Builder $subQuery) use ($group): void {
-                $group->rules->each(function (SmartPlaylistRule $rule) use ($subQuery): void {
-                    $this->buildQueryForRule($subQuery, $rule);
+            $query->$clause(static function (Builder $subQuery) use ($group): void {
+                $group->rules->each(static function (SmartPlaylistRule $rule) use ($subQuery): void {
+                    $subWhere = $rule->operator === SmartPlaylistRule::OPERATOR_IS_BETWEEN ? 'whereBetween' : 'where';
+                    $subQuery->$subWhere(...$rule->toCriteriaParameters());
                 });
             });
         });
 
-        return $query;
-    }
-
-    /**
-     * Some rules need to be driven by an additional "user" factor, for example play count, liked, or last played
-     * (basically everything related to interactions).
-     * For those, we create an additional "user_id" rule.
-     *
-     * @return Collection|array<SmartPlaylistRuleGroup>
-     */
-    public function addRequiresUserRules(Collection $ruleGroups, User $user): Collection
-    {
-        return $ruleGroups->map(function (SmartPlaylistRuleGroup $group) use ($user): SmartPlaylistRuleGroup {
-            $clonedGroup = clone $group;
-            $additionalRules = collect();
-
-            $group->rules->each(function (SmartPlaylistRule $rule) use ($additionalRules, $user): void {
-                foreach (self::USER_REQUIRING_RULE_PREFIXES as $modelPrefix) {
-                    if (starts_with($rule->model, $modelPrefix)) {
-                        $additionalRules->add($this->createRequiresUserRule($user, $modelPrefix));
-                    }
-                }
-            });
-
-            // Make sure all those additional rules are unique.
-            $clonedGroup->rules = $clonedGroup->rules->merge($additionalRules->unique('model')->collect());
-
-            return $clonedGroup;
-        });
-    }
-
-    private function createRequiresUserRule(User $user, string $modelPrefix): SmartPlaylistRule
-    {
-        return SmartPlaylistRule::create([
-            'model' => $modelPrefix . 'user_id',
-            'operator' => 'is',
-            'value' => [$user->id],
-        ]);
-    }
-
-    public function buildQueryForRule(Builder $query, SmartPlaylistRule $rule, ?string $model = null): Builder
-    {
-        if (!$model) {
-            $model = $rule->model;
-        }
-
-        $fragments = explode('.', $model, 2);
-
-        if (count($fragments) === 1) {
-            return $query->{$this->resolveWhereLogic($rule)}(
-                ...$this->parameterFactory->createParameters($model, $rule->operator, $rule->value)
-            );
-        }
-
-        // If the model is something like 'artist.name' or 'interactions.play_count', we have a subquery to deal with.
-        // We handle such a case with a recursive call which, in theory, should work with an unlimited level of nesting,
-        // though in practice we only have one level max.
-        return $query->whereHas(
-            $fragments[0],
-            fn (Builder $subQuery) => $this->buildQueryForRule($subQuery, $rule, $fragments[1])
-        );
-    }
-
-    /**
-     * Resolve the logic of a (sub)query base on the configured operator.
-     * Basically, if the operator is "between," we use "whereBetween". Otherwise, it's "where". Simple.
-     */
-    private function resolveWhereLogic(SmartPlaylistRule $rule): string
-    {
-        return $rule->operator === SmartPlaylistRule::OPERATOR_IS_BETWEEN ? 'whereBetween' : 'where';
+        return $query->orderBy('songs.title')->get();
     }
 }
