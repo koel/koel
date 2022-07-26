b3ee1ff5288f972057813b982edc708194438729
Phan An <me@phanan.net>
2022-07-26 23:05:43 +0200

    feat(test|api): add PlayCount tests

---
+++ tests/Feature/V6/PlayCountTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\V6;
+
+use App\Models\Interaction;
+
+class PlayCountTest extends TestCase
+{
+    public function testStore(): void
+    {
+        /** @var Interaction $interaction */
+        $interaction = Interaction::factory()->create([
+            'play_count' => 10,
+        ]);
+
+        $response = $this->postAsUser('/api/interaction/play', [
+            'song' => $interaction->song->id,
+        ], $interaction->user);
+
+        $response->assertJsonStructure([
+            'type',
+            'id',
+            'song_id',
+            'liked',
+            'play_count',
+        ]);
+
+        self::assertEquals(11, $interaction->refresh()->play_count);
+    }
+}
