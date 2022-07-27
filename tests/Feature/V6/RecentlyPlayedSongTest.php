c19ba06912eafcb2dbabeab63696f4b6e0cb0d1b
Phan An <me@phanan.net>
2022-07-27 11:31:53 +0200

    feat(test|api): add RecentlyPlayedSong tests

--- tests/Feature/V6/RecentlyPlayedSongTest.php
+++ tests/Feature/V6/RecentlyPlayedSongTest.php
@@ -2,6 +2,19 @@
 
 namespace Tests\Feature\V6;
 
+use App\Models\Interaction;
+use App\Models\User;
+
 class RecentlyPlayedSongTest extends TestCase
 {
+    public function testIndex(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+
+        Interaction::factory(5)->for($user)->create();
+
+        $this->getAs('api/songs/recently-played', $user)
+            ->assertJsonStructure(['*' => SongTest::JSON_STRUCTURE]);
+    }
 }
