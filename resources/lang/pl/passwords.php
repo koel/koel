4976739cdc125c24cd2bd1c157a9a2dfd4a77d3f
Daniel Kamiński <daniel.kaminski123@gmail.com>
2022-10-06 11:32:26 +0200

    Create passwords.php

---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Hasło musi mieć co najmniej 6 znaków i pasować do hasła potwierdzającego.',
+    'reset' => 'Twoje hasło zostało zresetowane!',
+    'sent' => 'Wysłaliśmy do Ciebie maila z linkiem do zresetowania hasła!',
+    'token' => 'Token resetowania hasła jest niepoprawny.',
+    'user' => "Nie możemy znaleźć użytkownika z podanym adresem mailowym.",
+];
