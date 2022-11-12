680ddb70988a9d0ff3967140e7f2228def77e9c0
Ali Agho <ali_fm2009@yahoo.com>
2022-11-12 12:22:09 +0100

    Testing relationship between Song and Interaction

--- tests/Unit/Models/SongTest.php
+++ tests/Unit/Models/SongTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Interaction;
 use App\Models\Song;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Tests\TestCase;
 
 class SongTest extends TestCase
@@ -21,5 +23,22 @@ class SongTest extends TestCase
         $song = Song::factory()->create(['lyrics' => "[00:00.00]Line 1\n[00:01.00]Line 2\n[00:02.00]Line 3"]);
 
         self::assertSame("Line 1\nLine 2\nLine 3", $song->lyrics);
+    }
+
+    public function testSongHasManyRelationshipWithInteractions()
+    {
+        /** @var Song $song */
+        $song = Song::factory()->create();
+
+        /** @var Interaction $interactions */
+        $interactions = Interaction::factory(3, ['song_id' => $song->id])->create();
+
+        $this->assertEquals($song->interactions->count(), $interactions->count());
+
+        $this->assertInstanceOf(Interaction::class, $song->interactions()->first());
+
+        $this->assertInstanceOf(HasMany::class, $song->interactions());
+
+        $this->assertInstanceOf(Song::class , $interactions->first()->song);
     }
 }
