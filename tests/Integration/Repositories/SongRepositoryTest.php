62d7a67ba00ec684430494f03de8eaf9f2ef1424
Ali Agho <ali_fm2009@yahoo.com>
2022-11-15 00:19:33 +0100

    Add two tests for SongRepository class

--- tests/Integration/Repositories/SongRepositoryTest.php
+++ tests/Integration/Repositories/SongRepositoryTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Integration\Repositories;
 
+use App\Models\Album;
 use App\Models\Song;
+use App\Models\User;
 use App\Repositories\SongRepository;
 use Tests\TestCase;
 
@@ -22,5 +24,36 @@ class SongRepositoryTest extends TestCase
         /** @var Song $song */
         $song = Song::factory()->create(['path' => 'foo']);
         self::assertSame($song->id, $this->songRepository->getOneByPath('foo')->id);
+    }
+
+    public function testGetAllHostedOnS3()
+    {
+        /** @var Song $song */
+        $song = Song::factory()->create(['path' => 's3://song/test']);
+
+        self::assertSame($song->s3_params,
+            $this->songRepository->getAllHostedOnS3()->first()->s3_params);
+    }
+
+    public function testGetRecentlyAdded()
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        /** @var Album $album */
+        $album = Album::factory([
+            'artist_id' => $user->id,
+        ])->create();
+
+        /** @var Song $songs */
+        $songs = Song::factory(10, [
+            'album_id' => $album->id,
+            'artist_id' => $album->artist->id
+        ])->create();
+
+        $this->assertEquals(10, $this->songRepository->getRecentlyAdded(10)->count());
+        $this->assertEquals(10, $this->songRepository->getRecentlyAdded(10, $user)->count());
+        $this->assertDatabaseCount(Song::class , 10);
     }
 }
