794ac48172531c0eb2b276a02f391af015ec61b8
Phan An <me@phanan.net>
2022-12-04 13:33:21 +0100

    fix: setting MySQL strict mode to true breaks migration (#1615)

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false, // setting this to true will cause IncreaseStringColumnsLength migration to fail
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
