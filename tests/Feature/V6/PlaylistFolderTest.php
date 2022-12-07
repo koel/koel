9a2d7e71c339bd7240aa2c69789af27e2af0d606
Ali Agho <ali_fm2009@yahoo.com>
2022-12-07 14:24:59 +0100

    Add a new tests for Playlist folder
    Testing created playlist for a folder

--- tests/Feature/V6/PlaylistFolderTest.php
+++ tests/Feature/V6/PlaylistFolderTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Feature\V6;
 
 use App\Models\PlaylistFolder;
+use App\Models\Song;
 use App\Models\User;
+
+use function Functional\pluck;
 
 class PlaylistFolderTest extends TestCase
 {
@@ -68,5 +71,44 @@ class PlaylistFolderTest extends TestCase
             ->assertForbidden();
 
         self::assertModelExists($folder);
+    }
+
+    public function testCreatePlaylistForFolder()
+    {
+        /** @var PlaylistFolder $folder */
+        $folder = PlaylistFolder::factory()->create();
+
+        $this->postAs('api/playlists', [
+            "name" => "NewForFolder",
+            "songs" => [],
+            "folder_id" => $folder->id
+        ], $folder->user)
+            ->assertJsonStructure([
+                'created_at',
+                'folder_id',
+                'id',
+                'is_smart',
+                'name',
+                'rules',
+                'type',
+                'user_id',
+            ]);
+    }
+
+    public function testCreatePlaylistForFolderWithSongs()
+    {
+        /** @var PlaylistFolder $folder */
+        $folder = PlaylistFolder::factory()->create();
+
+        $playlist = $this->postAs('api/playlists', [
+            "name" => "NewForFolder",
+            "songs" => Song::factory(3)->create()->pluck('id'),
+            "folder_id" => $folder->id
+        ], $folder->user)->decodeResponseJson();
+
+        $playlistId = json_decode($playlist->json, true)['id'];
+
+        $this->getAs('api/playlists/' . $playlistId . '/songs')
+        ->assertJsonCount(3);
     }
 }
