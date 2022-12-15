bf29f7e32e4cb6fd330ef3225864fd4234a93f0a
Phan An <me@phanan.net>
2022-12-16 00:16:36 +0700

    feat: use feather as the lyrics icon (#1633)

--- resources/assets/js/components/ui/ExtraDrawerTabHeader.spec.ts
+++ resources/assets/js/components/ui/ExtraDrawerTabHeader.spec.ts
@@ -2,13 +2,13 @@ import { expect, it } from 'vitest'
 import UnitTestCase from '@/__tests__/UnitTestCase'
 import { commonStore } from '@/stores'
 import { screen } from '@testing-library/vue'
-import ExtraPanelTabHeader from './ExtraPanelTabHeader.vue'
+import ExtraDrawerTabHeader from './ExtraDrawerTabHeader.vue'
 
 new class extends UnitTestCase {
   protected test () {
     it('renders tab headers', () => {
       commonStore.state.use_you_tube = false
-      this.render(ExtraPanelTabHeader)
+      this.render(ExtraDrawerTabHeader)
 
       ;['Lyrics', 'Artist information', 'Album information'].forEach(name => screen.getByRole('button', { name }))
       expect(screen.queryByRole('button', { name: 'Related YouTube videos' })).toBeNull()
@@ -16,13 +16,13 @@ new class extends UnitTestCase {
 
     it('has a YouTube tab header if using YouTube', () => {
       commonStore.state.use_you_tube = true
-      this.render(ExtraPanelTabHeader)
+      this.render(ExtraDrawerTabHeader)
 
       screen.getByRole('button', { name: 'Related YouTube videos' })
     })
 
     it('emits the selected tab value', async () => {
-      const { emitted } = this.render(ExtraPanelTabHeader)
+      const { emitted } = this.render(ExtraDrawerTabHeader)
 
       await this.user.click(screen.getByRole('button', { name: 'Lyrics' }))
 
