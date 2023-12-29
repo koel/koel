3deadeb174415a9f2d2d887a990d5ef3f8bad2c0
Phan An <me@phanan.net>
2023-12-29 16:10:31 +0100

    fix: split create and alter table clauses

--- database/migrations/2023_12_28_223335_create_queue_states_table.php
+++ database/migrations/2023_12_28_223335_create_queue_states_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
         Schema::create('queue_states', static function (Blueprint $table): void {
             $table->increments('id')->primary();
             $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
             $table->json('song_ids');
             $table->string('current_song_id', 36)->nullable();
-            $table->foreign('current_song_id')->references('id')->on('songs')->cascadeOnUpdate()->nullOnDelete();
             $table->unsignedInteger('playback_position')->default(0);
+        });
+
+        Schema::table('queue_states', static function (Blueprint $table): void {
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('current_song_id')->references('id')->on('songs')->cascadeOnUpdate()->nullOnDelete();
         });
     }
 };
