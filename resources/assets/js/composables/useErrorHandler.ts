182a6bce6b2d3ed5d62b401a42d6adb4b904b5af
Phan An <me@phanan.net>
2024-04-23 23:12:55 +0200

    refactor: useErrorHandler

--- resources/assets/js/composables/useErrorHandler.ts
+++ resources/assets/js/composables/useErrorHandler.ts
@@ -12,39 +12,27 @@ export const useErrorHandler = (driver: ErrorMessageDriver = 'toast') => {
   const { toastError } = useMessageToaster()
   const { showErrorDialog } = useDialogBox()
 
-  const showError = (message: string) => {
-    if (driver === 'toast') {
-      toastError(message)
-    } else {
-      showErrorDialog(message)
-    }
-  }
+  const showError = (message: string) => driver === 'toast' ? toastError(message) : showErrorDialog(message)
 
   const showGenericError = () => showError('An unknown error occurred.')
 
   const handleHttpError = (error: unknown, statusMessageMap: StatusMessageMap = {}) => {
     logger.error(error)
 
-    if (!axios.isAxiosError(error)) {
+    if (!axios.isAxiosError(error) || !error.response?.status) {
       return showGenericError()
     }
 
-    if (!error.response?.status || !Object.prototype.hasOwnProperty.call(statusMessageMap, error.response.status)) {
-      showError('An unknown error occurred.')
-      return
-    }
-
-    if (error.response.status === 422) {
+    if (
+      !Object.prototype.hasOwnProperty.call(statusMessageMap, error.response.status)
+      && error.response.status === 422
+    ) {
       return showError(parseValidationError(error.response.data)[0])
     }
 
     const messageOrClosure = statusMessageMap[error.response.status]
 
-    if (typeof messageOrClosure === 'string') {
-      showError(messageOrClosure)
-    } else {
-      return messageOrClosure()
-    }
+    return typeof messageOrClosure === 'string' ? showError(messageOrClosure) : messageOrClosure()
   }
 
   return {
