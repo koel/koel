a3f9715a128e6c7e0a7abf0565db902900fb4b02
Phan An <me@phanan.net>
2024-05-19 13:56:39 +0800

    fix: better handling of YouTube video loading failure

--- resources/assets/js/components/ui/youtube/YouTubeVideoList.vue
+++ resources/assets/js/components/ui/youtube/YouTubeVideoList.vue
@@ -9,12 +9,17 @@
       <Btn v-if="!loading" small @click.prevent="loadMore">Load More</Btn>
     </template>
 
-    <p v-if="loading" class="nope">Loading…</p>
+    <p v-if="loading">Loading…</p>
+
+    <p v-if="somethingWrong">
+      Failed to load videos.
+      <a @click.prevent="loadMore" href="#">Try again</a>
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineAsyncComponent, ref, toRefs, watch } from 'vue'
+import { computed, defineAsyncComponent, ref, toRefs, watch } from 'vue'
 import { youTubeService } from '@/services'
 import { useErrorHandler } from '@/composables'
 
@@ -35,13 +40,15 @@ const loadMore = async () => {
   try {
     const result = await youTubeService.searchVideosBySong(song.value, nextPageToken)
     nextPageToken = result.nextPageToken
-    videos.value.push(...result.items)
+    videos.value.push(...(result.items || []))
   } catch (error: unknown) {
     useErrorHandler().handleHttpError(error)
   } finally {
     loading.value = false
   }
 }
+
+const somethingWrong = computed(() => !loading.value && videos.value.length === 0)
 
 watch(song, () => {
   videos.value = []
