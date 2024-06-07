c42a814346beb2d8abc470598b7e1c1f84620fcf
Phan An <me@phanan.net>
2024-06-07 14:41:37 +0200

    fix: sidebar toggling on mobile

--- resources/assets/js/components/ui/SidebarMenuToggleButton.vue
+++ resources/assets/js/components/ui/SidebarMenuToggleButton.vue
@@ -1,14 +1,24 @@
 <template>
-  <ExtraDrawerButton class="block md:hidden" @click.prevent.stop="toggleSidebar">
-    <Icon :icon="faBars" />
+  <ExtraDrawerButton class="block md:hidden" @click.prevent="toggleSidebar">
+    <Icon :icon="faTimes" v-if="sidebarExpanded" fixed-width />
+    <Icon :icon="faBars" v-else fixed-width />
   </ExtraDrawerButton>
 </template>
 
 <script lang="ts" setup>
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { eventBus } from '@/utils'
 
 import ExtraDrawerButton from '@/components/layout/main-wrapper/extra-drawer/ExtraDrawerButton.vue'
+import { ref } from 'vue'
 
-const toggleSidebar = () => eventBus.emit('TOGGLE_SIDEBAR')
+const sidebarExpanded = ref(false)
+
+const toggleSidebar = () => {
+  // Since the sidebar will be hidden if we click outside the menu anyway, we don't actually need
+  // to emit the event to collapse it. We just need to toggle the state to show the
+  // icon accordingly.
+  if (!sidebarExpanded.value) eventBus.emit('TOGGLE_SIDEBAR')
+  sidebarExpanded.value = !sidebarExpanded.value
+}
 </script>
