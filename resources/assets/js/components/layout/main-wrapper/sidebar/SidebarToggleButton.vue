e2dbf3f89abfacd7a9e6314e59d6d9c5dca4d14d
Phan An <me@phanan.net>
2024-07-01 09:40:09 +0200

    feat: make sidebar toggle btn appear on hover only

--- resources/assets/js/components/layout/main-wrapper/sidebar/SidebarToggleButton.vue
+++ resources/assets/js/components/layout/main-wrapper/sidebar/SidebarToggleButton.vue
@@ -2,7 +2,7 @@
   <label
     class="btn-toggle hidden md:flex w-[24px] aspect-square absolute rounded-full -right-[12px] top-[27px] items-center
     justify-center z-10 text-k-text-secondary bg-k-bg-secondary border-[1.5px] border-white/20 cursor-pointer
-    hover:text-k-text-primary hover:bg-k-bg-secondary"
+    hover:text-k-text-primary hover:bg-k-highlight"
   >
     <input v-model="value" class="hidden" type="checkbox">
     <Icon v-if="value" :icon="faAngleLeft" />
