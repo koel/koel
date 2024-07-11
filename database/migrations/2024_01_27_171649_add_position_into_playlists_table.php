a84dba5e505f3751904694c49aced2bbecc859e4
Phan An <me@phanan.net>
2024-07-11 19:24:40 +0200

    fix: no need to pre-calculate playlist song order

--- database/migrations/2024_01_27_171649_add_position_into_playlists_table.php
+++ database/migrations/2024_01_27_171649_add_position_into_playlists_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,19 +10,6 @@ return new class extends Migration
     {
         Schema::table('playlist_song', static function (Blueprint $table): void {
             $table->unsignedInteger('position')->index()->default(0);
-        });
-
-        DB::table('playlists')->orderBy('id')->chunk(100, static function ($playlists): void {
-            foreach ($playlists as $playlist) {
-                DB::table('playlist_song')
-                    ->where('playlist_id', $playlist->id)
-                    ->update([
-                        'position' => DB::raw("(SELECT COUNT(id)
-                            FROM playlist_song p
-                            WHERE p.playlist_id = '$playlist->id'
-                            AND p.id <= playlist_song.id)"),
-                    ]);
-            }
         });
     }
 };
