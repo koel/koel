5d5e116e0a2c01b210bf7328e17676d84a193d9f
PBK-B <pbk-b@PBK6.cn>
2024-08-29 16:28:39 +0800

    fix(ui): load default avatar when avatar loading fails

--- resources/assets/js/components/user/UserAvatar.vue
+++ resources/assets/js/components/user/UserAvatar.vue
@@ -3,12 +3,14 @@
     :alt="`Avatar of ${user.name}`"
     :src="user.avatar"
     :title="user.name"
+    :onerror="user.avatar = defaultCover"
     class="rounded-full aspect-square bg-k-bg-primary object-cover"
   >
 </template>
 
 <script lang="ts" setup>
 import { toRefs } from 'vue'
+import { defaultCover } from '@/utils'
 
 const props = defineProps<{ user: Pick<User, 'name' | 'avatar'> }>()
 const { user } = toRefs(props)
