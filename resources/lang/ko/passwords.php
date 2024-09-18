63137e555981a703a16a855930602156a374f41b
DDinghoya <ddinghoya@gmail.com>
2024-09-18 23:21:11 +0900

    Create passwords.php

---
+++ resources/lang/ko/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '비밀번호는 최소 6자 이상이어야 하며 확인과 일치해야 합니다.',
+    'reset' => '비밀번호가 재설정되었습니다!',
+    'sent' => '비밀번호 재설정 링크를 이메일로 보냈습니다!',
+    'token' => '이 비밀번호 재설정 토큰은 유효하지 않습니다.',
+    'user' => '해당 이메일 주소로 사용자를 찾을 수 없습니다.',
+];
