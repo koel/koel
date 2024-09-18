7f9dd8f727ae35771bccc3870e7f97dea975f493
DDinghoya <ddinghoya@gmail.com>
2024-09-18 23:15:14 +0900

    Create auth.php

---
+++ resources/lang/ko/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '해당 자격 증명은 당사 기록과 일치하지 않습니다.',
+    'throttle' => '로그인 시도 횟수가 너무 많습니다. :seconds 초 후에 다시 시도하세요.',
+];
