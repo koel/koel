929749d0881697f61fb2cde5f39faae7d9b709ac
Tahmid Ziko <tahmidziko@gmail.com>
2024-11-06 15:52:36 +0600

    chore(config): update comment for Spotify integration (#1867)
    Co-authored-by: Phan An <me@phanan.net>

--- config/koel.php
+++ config/koel.php
@@ -79,10 +79,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Last.FM Integration
+    | Spotify Integration
     |--------------------------------------------------------------------------
     |
-    | See wiki on how to integrate with Last.FM
+    | Spotify Integration requires client ID and secret. 
     |
     */
 
