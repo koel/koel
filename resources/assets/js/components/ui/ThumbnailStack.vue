4a0c39f79c112809213682ec63d66626cb858d3f
Vineet kushwaha <kushwahavineet651@gmail.com>
2024-11-10 12:54:59 +0530

    feat: update album thumbnail on queue page

--- resources/assets/js/components/ui/ThumbnailStack.vue
+++ resources/assets/js/components/ui/ThumbnailStack.vue
@@ -1,14 +1,14 @@
 <template>
   <article
-    :class="layout"
+    :class="[layout, { playing: isPlaying }]"
     :style="{ backgroundImage: `url(${defaultCover})` }"
-    class="thumbnail-stack aspect-square overflow-hidden grid bg-cover bg-no-repeat"
+    class="thumbnail-stack aspect-square overflow-hidden grid bg-cover bg-no-repeat thumbnail-stack"
   >
     <span
       v-for="thumbnail in displayedThumbnails"
       :key="thumbnail"
       :style="{ backgroundImage: `url(${thumbnail}` }"
-      class="block will-change-transform w-full h-full bg-cover bg-no-repeat"
+      class=" album-thumb block will-change-transform w-full h-full bg-cover bg-no-repeat"
       data-testid="thumbnail"
     />
   </article>
@@ -19,11 +19,11 @@ import { take } from 'lodash'
 import { computed, toRefs } from 'vue'
 import defaultCover from '@/../img/covers/default.svg'
 
-const props = defineProps<{ thumbnails: string[] }>()
+const props = defineProps<{ thumbnails: string[], songs?: object }>()
 
 const defaultBackgroundImage = `url(${defaultCover})`
 
-const { thumbnails } = toRefs(props)
+const { thumbnails, songs } = toRefs(props)
 
 const displayedThumbnails = computed(() => {
   return thumbnails.value.length === 0
@@ -32,6 +32,10 @@ const displayedThumbnails = computed(() => {
 })
 
 const layout = computed<'single' | 'tiles'>(() => displayedThumbnails.value.length < 4 ? 'single' : 'tiles')
+
+const isPlaying = computed(() => {
+  return songs.value?.some(song => song.playback_state === 'Playing') || false
+})
 </script>
 
 <style lang="postcss" scoped>
@@ -40,6 +44,15 @@ article {
 
   &.tiles {
     @apply grid-cols-2;
+  }
+  &.playing .album-thumb {
+    @apply motion-reduce:animate-none;
+    animation: spin 30s linear infinite;
+  }
+}
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
