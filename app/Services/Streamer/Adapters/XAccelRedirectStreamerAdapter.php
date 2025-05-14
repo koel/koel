a55672bb030bdcb784095f468faed9249a89b9bd
Phan An <me@phanan.net>
2025-05-14 18:48:55 +0200

    chore: super minor grammar fix

--- app/Services/Streamer/Adapters/XAccelRedirectStreamerAdapter.php
+++ app/Services/Streamer/Adapters/XAccelRedirectStreamerAdapter.php
@@ -19,7 +19,7 @@ class XAccelRedirectStreamerAdapter extends LocalStreamerAdapter
         $relativePath = str_replace(Setting::get('media_path'), '', $path);
 
         // We send our media_path value as an 'X-Media-Root' header to downstream (nginx)
-        // It will then be use as `alias` in X-Accel config location block.
+        // It will then be used as `alias` in X-Accel config location block.
         // See nginx.conf.example.
         header('X-Media-Root: ' . Setting::get('media_path'));
         header("X-Accel-Redirect: /media/$relativePath");
