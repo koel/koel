67ea19d731cb1102a10d3bc716eaab27b1b99edd
Phan An <me@phanan.net>
2025-05-18 20:23:03 +0200

    fix: broken auditable type (#1927)

---
+++ database/migrations/2025_05_18_181238_change_auditable_id_type.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $connection = config('audit.drivers.database.connection', config('database.default'));
+        $table = config('audit.drivers.database.table', 'audits');
+
+        Schema::connection($connection)->table($table, static function (Blueprint $table): void {
+            $table->string('auditable_id')->change();
+        });
+    }
+};
