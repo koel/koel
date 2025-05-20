a9885bde58ff9bfaafd8729687ef3a90d06476e2
Phan An <me@phanan.net>
2025-05-20 21:52:33 +0200

    feat: useListSelection composable

--- resources/assets/js/components/song/media-browser/MediaBrowser.vue
+++ resources/assets/js/components/song/media-browser/MediaBrowser.vue
@@ -23,12 +23,12 @@
 </template>
 
 <script setup lang="ts">
-import { computed, nextTick, reactive, ref, toRefs } from 'vue'
-import { findIndex } from 'lodash'
+import { computed, nextTick, reactive, toRefs, watch } from 'vue'
 import { eventBus } from '@/utils/eventBus'
 import { useRouter } from '@/composables/useRouter'
 import { playbackService } from '@/services/playbackService'
 import { useDraggable } from '@/composables/useDragAndDrop'
+import { useListSelection } from '@/composables/useListSelection'
 import { isSong } from '@/utils/typeGuards'
 
 import VirtualScroller from '@/components/ui/VirtualScroller.vue'
@@ -47,8 +47,6 @@ const { items } = toRefs(props)
 const { go, url } = useRouter()
 const { startDragging } = useDraggable('browser-media')
 
-const lastSelectedRow = ref<MediaRow | null>(null)
-
 const rows = computed(() => {
   return items.value.map((item): MediaRow => {
     return reactive({
@@ -58,62 +56,57 @@ const rows = computed(() => {
   })
 })
 
+const {
+  select,
+  selectAll,
+  selectBetween,
+  clearSelection,
+  toggleSelected,
+  isSelected,
+  selected,
+  lastSelected,
+  reapplySelection,
+} = useListSelection(rows, (row: MediaRow) => isSong(row.item) ? 'item.id' : 'item.path')
+
+watch(items, () => reapplySelection(), { deep: true })
+
 const handleEnter = () => {
 }
 
-const clearSelection = () => rows.value.forEach(row => (row.selected = false))
-const selectAllRows = () => rows.value.forEach(row => (row.selected = true))
-const handleA = (event: KeyboardEvent) => (event.ctrlKey || event.metaKey) && selectAllRows()
-
-const selectedItems = computed(() => rows.value.filter(row => row.selected).map(row => row.item))
+const handleA = (event: KeyboardEvent) => (event.ctrlKey || event.metaKey) && selectAll()
+const selectedItems = computed(() => selected.value.map(row => row.item))
 const onlySongsSelected = computed(() => selectedItems.value.every(isSong))
 
 const onDragStart = async (row: MediaRow, event: DragEvent) => {
   // If the user is dragging an unselected row, clear the current selection.
-  if (!row.selected) {
+  if (!isSelected(row)) {
     clearSelection()
-    row.selected = true
+    select(row)
     await nextTick()
   }
 
   startDragging(event, selectedItems.value)
 }
 
-const toggleRow = (row: MediaRow) => {
-  row.selected = !row.selected
-  lastSelectedRow.value = row
-}
-
-const selectRowsBetween = (first: MediaRow, second: MediaRow) => {
-  const firstIndex = Math.max(0, findIndex(rows.value, row => row.item.id === first.item.id))
-  const secondIndex = Math.max(0, findIndex(rows.value, row => row.item.id === second.item.id))
-  const indexes = [firstIndex, secondIndex]
-  indexes.sort((a, b) => a - b)
-
-  for (let i = indexes[0]; i <= indexes[1]; ++i) {
-    rows.value[i].selected = true
-  }
-}
-
 const onClick = (row: MediaRow, event: MouseEvent) => {
   // If we're on a touch device, or if Ctrl/Cmd key is pressed, just toggle selection.
   if (isMobile.any) {
-    toggleRow(row)
+    toggleSelected(row)
     return
   }
 
   if (event.ctrlKey || event.metaKey) {
-    toggleRow(row)
+    toggleSelected(row)
   }
 
   if (event.button === 0) {
     if (!(event.ctrlKey || event.metaKey || event.shiftKey)) {
       clearSelection()
-      toggleRow(row)
+      toggleSelected(row)
     }
 
-    if (event.shiftKey && lastSelectedRow.value) {
-      selectRowsBetween(lastSelectedRow.value, row)
+    if (event.shiftKey && lastSelected.value) {
+      selectBetween(lastSelected.value, row)
     }
   }
 }
@@ -129,7 +122,7 @@ const onDblclick = (row: MediaRow) => {
 const onContextMenu = async (row: MediaRow, event: MouseEvent) => {
   if (!row.selected) {
     clearSelection()
-    toggleRow(row)
+    toggleSelected(row)
 
     // awaiting a next tick so that the selected items are collected properly
     await nextTick()
--- resources/assets/js/composables/useListSelection.ts
+++ resources/assets/js/composables/useListSelection.ts
@@ -1,34 +1,84 @@
 import type { Reactive, Ref } from 'vue'
+import { computed, ref } from 'vue'
+import { findIndex, get } from 'lodash'
 
-type Selectable = Reactive<Record<any, any> & { selected: boolean }>
+type Selectable<T> = Reactive<T & { selected: boolean }>
 
-export const useListSelection = (items: Ref<Selectable[]>) => {
-  const select = (item: Selectable) => (item.selected = true)
-  const deselect = (item: Selectable) => (item.selected = false)
-  const selectAll = () => items.value.forEach(item => (item.selected = true))
-  const deselectAll = () => items.value.forEach(item => (item.selected = false))
-  const toggleSelected = (item: Selectable) => (item.selected = !item.selected)
+export const useListSelection = <T>(
+  selectables: Ref<Array<Selectable<T>>>,
+  idPath: string | ((s: Selectable<T>) => string) = 'item.id',
+) => {
+  const lastSelected = ref<Selectable<T> | null>(null)
 
-  const selectBetween = (first: Selectable, second: Selectable, indexFinderFn: (selectable: Selectable) => number) => {
-    // const firstIndex = Math.max(0, findIndex(items.value, row => row.playable.id === first.playable.id))
-    // const secondIndex = Math.max(0, findIndex(rows.value, row => row.playable.id === second.playable.id))
-    const firstIndex = Math.max(0, indexFinderFn(first))
-    const secondIndex = Math.max(0, indexFinderFn(second))
+  // Keep track of selected items by their ID, so that when the list is updated (e.g., items are removed or added,
+  // the list is sorted, or infinite loading triggered), we can reapply the selection.
+  const selectedIds = new Map()
+
+  const resolveIdPath = (selectable: Selectable<T>) => typeof idPath === 'function' ? idPath(selectable) : idPath
+
+  const select = (selectable: Selectable<T>) => {
+    selectable.selected = true
+    lastSelected.value = selectable
+    selectedIds.set(get(selectable, resolveIdPath(selectable)), true)
+  }
+
+  const deselect = (selectable: Selectable<T>) => {
+    selectable.selected = false
+    lastSelected.value = selectable // we still consider it as "last selected" so that we can "select between"
+    selectedIds.delete(get(selectable, resolveIdPath(selectable)))
+  }
+
+  const selectAll = () => selectables.value.forEach(select)
+  const deselectAll = () => selectables.value.forEach(deselect)
+  const isSelected = (item: Selectable<T>) => item.selected
+  const selected = computed(() => selectables.value.filter(isSelected))
+
+  const toggleSelected = (selectable: Selectable<T>) => {
+    if (isSelected(selectable)) {
+      deselect(selectable)
+    } else {
+      select(selectable)
+    }
+
+    lastSelected.value = selectable
+  }
+
+  const selectBetween = (first: Selectable<T>, second: Selectable<T>) => {
+    const firstIndex = Math.max(
+      0,
+      findIndex(selectables.value, s => get(s, resolveIdPath(s)) === get(first, resolveIdPath(first))),
+    )
+
+    const secondIndex = Math.max(
+      0,
+      findIndex(selectables.value, s => get(s, resolveIdPath(s)) === get(second, resolveIdPath(second))),
+    )
+
     const indexes = [firstIndex, secondIndex]
     indexes.sort((a, b) => a - b)
 
     for (let i = indexes[0]; i <= indexes[1]; ++i) {
-      items.value[i].selected = true
+      select(selectables.value[i])
     }
+  }
+
+  const reapplySelection = () => {
+    selectables.value.forEach(selectable => {
+      // Don't use select() here, as it will set lastSelected and case other side effects.
+      selectable.selected = selectedIds.has(get(selectable, resolveIdPath(selectable)))
+    })
   }
 
   return {
     select,
-    deselect,
+    isSelected,
     selectAll,
     deselectAll,
     clearSelection: deselectAll,
     toggleSelected,
     selectBetween,
+    reapplySelection,
+    selected,
+    lastSelected,
   }
 }
