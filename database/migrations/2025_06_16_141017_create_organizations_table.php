0ac37a432c8e74be3f41aa5b893b8bccdd873829
Phan An <me@phanan.net>
2025-06-17 10:17:15 +0200

    fix: org cascade on delete

--- database/migrations/2025_06_16_141017_create_organizations_table.php
+++ database/migrations/2025_06_16_141017_create_organizations_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
             $table->foreign('organization_id')
                 ->references('id')
                 ->on('organizations')
-                ->nullOnDelete()
+                ->cascadeOnDelete()
                 ->cascadeOnUpdate();
         });
 
