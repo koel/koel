676c594610651401e4470c4d73fb87b963c3efdb
Phan An <me@phanan.net>
2025-07-02 13:02:52 +0700

    fix: artist/album info tab missing

--- resources/assets/js/components/profile-preferences/MusicBrainzIntegration.spec.ts
+++ resources/assets/js/components/profile-preferences/MusicBrainzIntegration.spec.ts
@@ -1,14 +1,14 @@
 import { expect, it } from 'vitest'
 import UnitTestCase from '@/__tests__/UnitTestCase'
 import { commonStore } from '@/stores/commonStore'
-import Component from './LastfmIntegration.vue'
+import Component from './MusicBrainzIntegration.vue'
 
 new class extends UnitTestCase {
   protected test () {
     it.each<[boolean, boolean]>([[false, false], [false, true], [true, false], [true, true]])(
-      'renders proper content with Last.fm integration status %s, current user admin status %s',
-      (useLastfm, isAdmin) => {
-        commonStore.state.uses_last_fm = useLastfm
+      'renders proper content with MusicBrainz integration status %s, current user admin status %s',
+      (useMusicBrainz, isAdmin) => {
+        commonStore.state.uses_musicbrainz = useMusicBrainz
 
         if (isAdmin) {
           this.beAdmin()
---
+++ resources/assets/js/components/profile-preferences/MusicBrainzIntegration.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="text-k-text-secondary">
+    <h3 class="text-2xl mb-2 flex items-center gap-2">
+      <span class="mr-2 text-[#1db954]">
+        <img :src="musicbrainzLogo" alt="MusicBrainz Logo" height="20" width="20">
+      </span>
+      MusicBrainz Integration
+    </h3>
+
+    <div v-if="useMusicBrainz">
+      <p>
+        MusicBrainz integration is enabled.
+        Koel will attempt to retrieve album and artist information from MusicBrainz (and Wikipedia) when a song is
+        played, if needed.
+      </p>
+    </div>
+    <div v-else>
+      <p>
+        MusicBrainz integration is not enabled.
+        <span v-if="isAdmin" data-testid="spotify-admin-instruction">
+          Check
+          <a href="https://docs.koel.dev/service-integrations#musicbrainz-wikipedia" target="_blank">Documentation</a>
+          for integration instructions.
+        </span>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import musicbrainzLogo from '@/../img/logos/musicbrainz.svg'
+
+import { useAuthorization } from '@/composables/useAuthorization'
+import { useThirdPartyServices } from '@/composables/useThirdPartyServices'
+
+const { isAdmin } = useAuthorization()
+const { useMusicBrainz } = useThirdPartyServices()
+</script>
