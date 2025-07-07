b033fe08da505774b1bbe0958729ab4d156ac514
Phan An <me@phanan.net>
2025-07-07 22:51:59 +0700

    fix: upload user avatar

--- resources/assets/js/components/user/UserAvatar.vue
+++ resources/assets/js/components/user/UserAvatar.vue
@@ -1,18 +1,17 @@
 <template>
   <img
     :alt="`Avatar of ${user.name}`"
-    :src="avatar"
+    :src="user.avatar"
     :title="user.name"
     class="object-cover rounded-full aspect-square bg-k-bg-primary"
-    @error="avatar = defaultCover"
+    @error="user.avatar = defaultCover"
   >
 </template>
 
 <script lang="ts" setup>
-import { ref, toRefs } from 'vue'
+import { toRefs } from 'vue'
 import defaultCover from '@/../img/covers/default.svg'
 
 const props = defineProps<{ user: Pick<User, 'name' | 'avatar'> }>()
 const { user } = toRefs(props)
-const avatar = ref(user.value.avatar)
 </script>
