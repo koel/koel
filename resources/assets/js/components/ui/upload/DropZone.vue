b044a9ef3d18137b68f6e5afd68947cb13c46555
Phan An <me@phanan.net>
2025-09-10 10:38:42 +0700

    chore: make dropzone fullscreen

--- resources/assets/js/components/ui/upload/DropZone.vue
+++ resources/assets/js/components/ui/upload/DropZone.vue
@@ -2,23 +2,22 @@
   <OnClickOutside @trigger="close">
     <div
       v-if="allowsUpload && mediaPathSetUp"
-      :class="{ droppable }"
-      class="drop-zone h-[256px] max-h-[66vh] aspect-square outline-4 outline-dashed outline-gray-300
-      fixed z-10 top-0 left-0 rounded-3xl bg-black/40 flex flex-col-reverse items-center justify-center
-      overflow-hidden duration-200"
+      class="drop-zone w-screen h-screen fixed z-50 top-0 left-0 rounded-3xl bg-black/40
+      flex items-center justify-center overflow-hidden duration-200"
       @dragleave="onDropLeave"
       @dragover="onDragOver"
       @drop="onDrop"
     >
-      <h3 class="text-3xl mt-4 font-extralight">Drop to upload</h3>
-      <Icon :icon="faUpload" size="6x" />
+      <div class="pointer-events-none flex flex-col items-center justify-center gap-4">
+        <Icon :icon="faUpload" size="6x" />
+        <h3 class="text-3xl font-extralight">Drop to upload</h3>
+      </div>
     </div>
   </OnClickOutside>
 </template>
 
 <script lang="ts" setup>
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
-import { ref } from 'vue'
 import { OnClickOutside } from '@vueuse/components'
 import { useUpload } from '@/composables/useUpload'
 
@@ -26,34 +25,18 @@ const emit = defineEmits<{ (e: 'close'): void }>()
 
 const { allowsUpload, mediaPathSetUp, handleDropEvent } = useUpload()
 
-const droppable = ref(false)
-
-const onDropLeave = () => (droppable.value = false)
-
 const onDragOver = (event: DragEvent) => {
   if (!event.dataTransfer?.types.includes('Files')) {
     return false
   }
+
   event.preventDefault()
-  droppable.value = true
 }
 
 const onDrop = async (event: DragEvent) => {
   event.preventDefault()
-  droppable.value = false
   await handleDropEvent(event)
 }
 
 const close = () => emit('close')
 </script>
-
-<style lang="postcss" scoped>
-.drop-zone {
-  transform: translate(calc(50vw - 50%), calc(50vh - 50%));
-
-  &.droppable {
-    @apply h-[384px] outline-white bg-black/90;
-    box-shadow: 0 0 0 999rem rgba(0, 0, 0, 0.7);
-  }
-}
-</style>
