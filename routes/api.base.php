9da3e5d722ece9e704db32673c9cf0a5e6a067e1
Yihang Wang <16917636+WangYihang@users.noreply.github.com>
2025-09-12 17:55:42 +0800

    refactor: restrict user API routes to specific actions only (#2098) (#2101)

--- routes/api.base.php
+++ routes/api.base.php
@@ -173,10 +173,10 @@ Route::prefix('api')->middleware('api')->group(static function (): void {
         Route::get('genres', [GenreController::class, 'index']);
         Route::get('genres/{genre}', [GenreController::class, 'show']);
 
-        Route::apiResource('users', UserController::class);
+        Route::apiResource('users', UserController::class)->except('show');
 
         // User and user profile routes
-        Route::apiResource('user', UserController::class);
+        Route::apiResource('user', UserController::class)->except('show');
         Route::get('me', [ProfileController::class, 'show']);
         Route::put('me', [ProfileController::class, 'update']);
         Route::patch('me/preferences', UpdateUserPreferenceController::class);
