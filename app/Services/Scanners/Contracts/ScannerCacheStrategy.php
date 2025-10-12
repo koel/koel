5442d15d4ce56c3c04f431b9da7497395ca470b1
DDEV User <nobody@example.com>
2025-10-12 18:16:07 +0200

    fix: use in-memory cache for ScannerCacheStrategy
    Fixes a bug where, if a song is deleted along with its
    artist/album and immediately re-uploaded, the cache
    provides an invalid state.
    
    @see https://github.com/koel/koel/pull/2114

--- app/Services/Scanners/Contracts/ScannerCacheStrategy.php
+++ app/Services/Scanners/Contracts/ScannerCacheStrategy.php
@@ -3,14 +3,11 @@
 namespace App\Services\Scanners\Contracts;
 
 use Closure;
-use DateInterval;
-use DateTimeInterface;
 
 interface ScannerCacheStrategy
 {
     public function remember(
         string $key,
-        Closure|DateTimeInterface|DateInterval|int|null $ttl,
         Closure $callback,
     ): mixed;
 }
--- app/Services/Scanners/ScannerCacheStrategy.php
+++ app/Services/Scanners/ScannerCacheStrategy.php
@@ -4,17 +4,32 @@ namespace App\Services\Scanners;
 
 use App\Services\Scanners\Contracts\ScannerCacheStrategy as ScannerCacheStrategyContract;
 use Closure;
-use DateInterval;
-use DateTimeInterface;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Collection;
 
 class ScannerCacheStrategy implements ScannerCacheStrategyContract
 {
-    public function remember(
-        string $key,
-        DateInterval|DateTimeInterface|int|Closure|null $ttl,
-        Closure $callback,
-    ): mixed {
-        return Cache::remember($key, $ttl, $callback);
+    private const MAX_CACHE_SIZE = 1000;
+
+    /** @var Collection<string, mixed> */
+    private Collection $cache;
+
+    public function __construct()
+    {
+        $this->cache = new Collection();
+    }
+
+    public function remember(string $key, Closure $callback): mixed
+    {
+        if ($this->cache->has($key)) {
+            return $this->cache[$key];
+        }
+
+        if ($this->cache->count() > self::MAX_CACHE_SIZE) {
+            $this->cache->shift();
+        }
+
+        $this->cache[$key] = $callback();
+
+        return $this->cache[$key];
     }
 }
