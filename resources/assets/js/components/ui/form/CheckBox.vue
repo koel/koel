434bfe4781eb76ccad4fb0d7abc6095e3bf96152
Phan An <me@phanan.net>
2025-10-24 12:58:18 +0200

    chore(ui): ensure checkbox contrast (#2150)

--- resources/assets/js/components/ui/form/CheckBox.vue
+++ resources/assets/js/components/ui/form/CheckBox.vue
@@ -3,7 +3,7 @@
     v-model="value"
     :checked="value"
     class="relative align-bottom inline-block w-[32px] h-[20px] bg-gray-400 rounded-full shadow-inner cursor-pointer transition-all duration-200 ease-in-out mr-2
-      after:h-[16px] after:aspect-square after:absolute after:bg-white after:top-[2px] after:left-[2px] after:rounded-full after:transition-left after:duration-200 after:ease-in-out
+      after:h-[16px] after:aspect-square after:absolute after:bg-k-highlight-fg after:top-[2px] after:left-[2px] after:rounded-full after:transition-left after:duration-200 after:ease-in-out
       checked:bg-k-highlight checked:after:left-[14px]"
     type="checkbox"
     v-bind="$attrs"
