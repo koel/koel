7f16886294cd7536f371a5c2413bbf4b4faff62d
Phan An <me@phanan.net>
2026-03-07 01:31:51 +0700

    test: add tests for SetUserRoleCommand

---
+++ tests/Feature/Commands/SetUserRoleCommandTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Commands;
+
+use App\Models\User;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+use function Tests\create_user;
+
+class SetUserRoleCommandTest extends TestCase
+{
+    #[Test]
+    public function setUserRole(): void
+    {
+        $user = create_user(['email' => 'jane@example.com']);
+
+        $this
+            ->artisan('koel:admin:set-user-role', ['email' => 'jane@example.com'])
+            ->expectsChoice('What role should the user have?', 'admin', [
+                'admin' => 'Admin',
+                'user' => 'User',
+            ])
+            ->assertSuccessful();
+
+        $user->refresh();
+        self::assertTrue($user->hasRole('admin'));
+    }
+
+    #[Test]
+    public function failForNonExistentUser(): void
+    {
+        $this->artisan('koel:admin:set-user-role', ['email' => 'nobody@example.com'])->assertFailed();
+    }
+}
