57588a203b20a106a6b51e990dc4850e7a043406
Phan An <me@phanan.net>
2026-03-31 00:20:05 +0200

    test: properly assert similar_songs structure in OverviewTest

--- tests/Feature/OverviewTest.php
+++ tests/Feature/OverviewTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature;
 use App\Http\Resources\AlbumResource;
 use App\Http\Resources\ArtistResource;
 use App\Http\Resources\SongResource;
+use App\Models\Artist;
 use App\Models\Interaction;
+use App\Models\Song;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -17,8 +19,17 @@ class OverviewTest extends TestCase
     public function index(): void
     {
         $user = create_user();
+        $artist = Artist::factory()->createOne();
 
-        Interaction::factory()->for($user)->createMany(20);
+        // Create songs by the same artist so that similar_songs has results
+        $songs = Song::factory()->for($artist)->createMany(10);
+
+        foreach ($songs as $song) {
+            Interaction::factory()
+                ->for($user)
+                ->for($song)
+                ->createOne();
+        }
 
         $this->getAs('api/overview', $user)->assertJsonStructure([
             'most_played_songs' => [0 => SongResource::JSON_STRUCTURE],
@@ -30,7 +41,7 @@ class OverviewTest extends TestCase
             'most_played_albums' => [0 => AlbumResource::JSON_STRUCTURE],
             'least_played_songs' => [0 => SongResource::JSON_STRUCTURE],
             'random_songs' => [0 => SongResource::JSON_STRUCTURE],
-            'similar_songs',
+            'similar_songs' => [0 => SongResource::JSON_STRUCTURE],
         ]);
     }
 }
