a06428f4297b6352bf705b1ee5943e2b738cc449
Phan An <me@phanan.net>
2026-05-14 19:35:25 +0200

    Revert "fix(tailwind-v4): silence two prod-build CSS warnings (#2467)"
    This reverts commit 97e6b5c58c7b0021690222f015a49e7ca843e208.

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,9 @@ export default {
         'k-sidebar-width': 'var(--sidebar-width)',
         'k-side-sheet-width': 'var(--side-sheet-width)',
       },
+      screens: {
+        'no-hover': { 'raw': '(hover: none)' },
+      }
     },
     variants: {
       extend: {
