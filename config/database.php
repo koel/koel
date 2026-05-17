caed7b6ff07727065ab61f48aa23c9b1ff551c9c
Phan An <me@phanan.net>
2026-05-17 09:08:18 +0200

    fix: sqlite connection honors DB_DATABASE instead of hardcoding :memory:

--- config/database.php
+++ config/database.php
@@ -4,38 +4,38 @@ use Illuminate\Support\Str;
 
 return [
     /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Default Database Connection Name
+     |--------------------------------------------------------------------------
+     |
+     | Here you may specify which of the database connections below you wish
+     | to use as your default connection for all database work. Of course
+     | you may use many connections at once using the Database library.
+     |
+     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Database Connections
+     |--------------------------------------------------------------------------
+     |
+     | Here are each of the database connections setup for your application.
+     | Of course, examples of configuring each database platform that is
+     | supported by Laravel is shown below to make development simple.
+     |
+     |
+     | All database work in Laravel is done through the PHP PDO facilities
+     | so make sure you have the driver for your particular database of
+     | choice installed on your machine before you begin development.
+     |
+     */
 
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => ':memory:',
+            'database' => env('DB_DATABASE', ':memory:'),
             'prefix' => '',
         ],
 
@@ -66,9 +66,10 @@ return [
             'prefix_indexes' => true,
             'strict' => false, // setting this to true will cause IncreaseStringColumnsLength migration to fail
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ]) : [],
         ],
 
         'mariadb' => [
@@ -86,9 +87,10 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ]) : [],
         ],
 
         'pgsql' => [
@@ -148,31 +150,30 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Migration Repository Table
+     |--------------------------------------------------------------------------
+     |
+     | This table keeps track of all the migrations that have already run for
+     | your application. Using this information, we can determine which of
+     | the migrations on disk haven't actually been run in the database.
+     |
+     */
 
     'migrations' => 'migrations',
 
     /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Redis Databases
+     |--------------------------------------------------------------------------
+     |
+     | Redis is an open source, fast, and advanced key-value store that also
+     | provides a richer set of commands than a typical key-value systems
+     | such as APC or Memcached. Laravel makes it easy to dig right in.
+     |
+     */
 
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
@@ -197,6 +198,5 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
     ],
 ];
