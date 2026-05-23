ebfe1264717868b0933423ef877f76ee0ee2720e
Phan An <me@phanan.net>
2026-05-23 19:25:17 +0200

    refactor: import SocialiteManager instead of using FQCN

--- app/Providers/SocialiteServiceProvider.php
+++ app/Providers/SocialiteServiceProvider.php
@@ -3,17 +3,19 @@
 namespace App\Providers;
 
 use App\Socialite\OpenIDConnect\Provider as OpenIDConnectProvider;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Socialite\Contracts\Factory as SocialiteFactory;
+use Laravel\Socialite\SocialiteManager;
 
 class SocialiteServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        /** @var \Laravel\Socialite\SocialiteManager $socialite */
+        /** @var SocialiteManager $socialite */
         $socialite = $this->app->make(SocialiteFactory::class);
 
-        $socialite->extend('oidc', static function ($app) use ($socialite) {
+        $socialite->extend('oidc', static function (Application $app) use ($socialite) {
             return $socialite->buildProvider(OpenIDConnectProvider::class, config('services.oidc'));
         });
     }
