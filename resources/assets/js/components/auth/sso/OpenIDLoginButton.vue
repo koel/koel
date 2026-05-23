835c7dffd99edde0919424af7c2fab2c7b027665
Phan An <me@phanan.net>
2026-05-23 20:11:04 +0200

    feat: bundle OIDC provider logos locally instead of fetching from CDN

--- resources/assets/js/components/auth/sso/OpenIDLoginButton.vue
+++ resources/assets/js/components/auth/sso/OpenIDLoginButton.vue
@@ -4,37 +4,43 @@
     type="button"
     @click.prevent="loginWithOpenID"
   >
-    <img v-if="brandIconUrl" :src="brandIconUrl" :alt="label" class="w-4 h-4" @error="brandIconUrl = ''" />
+    <img v-if="brandIconUrl" :src="brandIconUrl" :alt="label" class="w-4 h-4" />
     <Icon v-else :icon="faKey" />
     <span class="text-sm">{{ label }}</span>
   </button>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
 import { faKey } from '@fortawesome/free-solid-svg-icons'
 import { openPopup } from '@/utils/helpers'
 
+import appleLogo from '@/../img/logos/sso/apple.svg'
+import autheliaLogo from '@/../img/logos/sso/authelia.svg'
+import authentikLogo from '@/../img/logos/sso/authentik.svg'
+import awsLogo from '@/../img/logos/sso/aws.svg'
+import githubLogo from '@/../img/logos/sso/github.svg'
+import gitlabLogo from '@/../img/logos/sso/gitlab.svg'
+import keycloakLogo from '@/../img/logos/sso/keycloak.svg'
+import microsoftLogo from '@/../img/logos/sso/microsoft.svg'
+import oktaLogo from '@/../img/logos/sso/okta.svg'
+import zitadelLogo from '@/../img/logos/sso/zitadel.svg'
+
 const label = window.KOEL.sso_oidc_label || 'OpenID Connect'
 
-const brandSlugs: Array<[RegExp, string]> = [
-  [/authentik/i, 'authentik'],
-  [/authelia/i, 'authelia'],
-  [/keycloak/i, 'keycloak'],
-  [/zitadel/i, 'zitadel'],
-  [/okta/i, 'okta'],
-  [/auth0/i, 'auth0'],
-  [/github/i, 'github'],
-  [/gitlab/i, 'gitlab'],
-  [/microsoft|azure|entra/i, 'microsoft'],
-  [/apple/i, 'apple'],
-  [/cognito|amazon|aws/i, 'aws'],
+const brandIcons: Array<[RegExp, string]> = [
+  [/authentik/i, authentikLogo],
+  [/authelia/i, autheliaLogo],
+  [/keycloak/i, keycloakLogo],
+  [/zitadel/i, zitadelLogo],
+  [/okta/i, oktaLogo],
+  [/github/i, githubLogo],
+  [/gitlab/i, gitlabLogo],
+  [/microsoft|azure|entra/i, microsoftLogo],
+  [/apple/i, appleLogo],
+  [/cognito|amazon|aws/i, awsLogo],
 ]
 
-const matchedSlug = brandSlugs.find(([pattern]) => pattern.test(label))?.[1]
-const brandIconUrl = ref(
-  matchedSlug ? `https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/${matchedSlug}.svg` : '',
-)
+const brandIconUrl = brandIcons.find(([pattern]) => pattern.test(label))?.[1] ?? ''
 
 const emit = defineEmits<{
   (e: 'success', data: any): void
