11018686bf32ffd369d703bd1fc632de77fab690
parmysssef <parmyssseifollahy@gmail.com>
2026-05-28 16:52:50 +0300

    Update api.base.php
    Use Song duration helper in library statistics route

--- routes/api.base.php
+++ routes/api.base.php
@@ -247,7 +247,7 @@ Route::get('library/stats', function () {
       'songs' => Song::count(),
         'albums' => Album::count(),
        'artists' => Artist::count(),
-        'duration' => Song::sum('length'),
+       'duration' => Song::getTotalDuration(),
     ]);
 });
             // License routes
