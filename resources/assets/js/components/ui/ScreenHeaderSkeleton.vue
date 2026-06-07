dda6b4b435e1acfdd77628d8026d38374c58fc36
Phan An <me@phanan.net>
2026-06-07 13:48:19 +0200

    fix: make ScreenHeaderSkeleton responsive on mobile

--- resources/assets/js/components/ui/ScreenHeaderSkeleton.vue
+++ resources/assets/js/components/ui/ScreenHeaderSkeleton.vue
@@ -1,11 +1,13 @@
 <template>
-  <header class="skeleton screen-header expanded flex items-end relative content-stretch p-8">
-    <aside class="w-[192px] aspect-square rounded-lg block mr-5 pulse" />
+  <header
+    class="skeleton screen-header expanded gap-4 min-h-0 md:min-h-full flex items-end shrink-0 relative content-stretch p-6 border-b border-b-k-fg-5"
+  >
+    <aside class="hidden md:block w-[192px] aspect-square rounded-lg pulse" />
 
-    <main class="flex flex-col items-start flex-1 gap-5">
-      <h1 class="h-16 w-4/5 pulse" />
+    <main class="flex flex-col items-start flex-1 gap-3 md:gap-5">
+      <h1 class="h-[clamp(1.8rem,3vw,4rem)] w-4/5 pulse" />
       <p class="meta h-[1.2rem] w-2/5 pulse" />
-      <p class="controls h-10 rounded-sm w-1/4 pulse" />
+      <p class="controls h-8 md:h-10 rounded-sm w-1/3 md:w-1/4 pulse" />
     </main>
   </header>
 </template>
