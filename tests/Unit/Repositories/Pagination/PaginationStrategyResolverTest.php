94c7b49aa5b9ac64e988ab4defd6a7e75f5173c4
Phan An <me@phanan.net>
2026-06-14 18:37:54 +0200

    test: inline cursor-present cases with TestWith

--- tests/Unit/Repositories/Pagination/PaginationStrategyResolverTest.php
+++ tests/Unit/Repositories/Pagination/PaginationStrategyResolverTest.php
@@ -6,14 +6,15 @@ use App\Repositories\Pagination\CursorStrategy;
 use App\Repositories\Pagination\OffsetStrategy;
 use App\Repositories\Pagination\PaginationStrategyResolver;
 use Illuminate\Http\Request;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
 use Tests\TestCase;
 
 class PaginationStrategyResolverTest extends TestCase
 {
     #[Test]
-    #[DataProvider('cursorPresentProvider')]
+    #[TestWith(['eyJpZCI6MX0'])]
+    #[TestWith([''])]
     public function resolvesCursorStrategyWhenCursorPresent(string $cursor): void
     {
         $strategy = PaginationStrategyResolver::resolve(self::createRequest(['cursor' => $cursor]));
@@ -27,13 +28,6 @@ class PaginationStrategyResolverTest extends TestCase
         $strategy = PaginationStrategyResolver::resolve(self::createRequest());
 
         self::assertInstanceOf(OffsetStrategy::class, $strategy);
-    }
-
-    /** @return iterable<string, array{string}> */
-    public static function cursorPresentProvider(): iterable
-    {
-        yield 'with value' => ['eyJpZCI6MX0'];
-        yield 'empty' => [''];
     }
 
     /** @param array<string, string> $queryParams */
