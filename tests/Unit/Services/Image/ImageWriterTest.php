3201df99f58a4bcae19fb2b1c3a1479f2e8f1e24
Phan An <me@phanan.net>
2026-09-17 21:36:17 +0700

    fix: download images from hosts that refuse a browser user agent (#2693)

--- tests/Unit/Services/Image/ImageWriterTest.php
+++ tests/Unit/Services/Image/ImageWriterTest.php
@@ -4,7 +4,9 @@ namespace Tests\Unit\Services\Image;
 
 use App\Services\Image\ImageWriter;
 use App\Values\ImageWritingConfig;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Image;
 use Illuminate\Support\Str;
 use PHPUnit\Framework\Attributes\Test;
@@ -27,6 +29,44 @@ class ImageWriterTest extends TestCase
         // Re-encode before measuring: the written format may be one that getimagesize() can't read,
         // even when the image driver is perfectly able to encode it.
         self::assertSame($sourceWidth, Image::fromPath($destination)->toPng()->width());
+
+        File::delete($destination);
+    }
+
+    #[Test]
+    public function retriesAsKoelWhenAHostRefusesTheBrowserUserAgent(): void
+    {
+        $image = File::get(test_path('fixtures/cover.png'));
+
+        Http::fake(static fn (Request $request) => (
+            $request->hasHeader('User-Agent', koel_user_agent()) ? Http::response($image, 200, [
+                    'Content-Type' => 'image/png',
+                ]) : Http::response('Too Many Requests', 429)
+        ));
+
+        $destination = sys_get_temp_dir() . '/' . Str::uuid() . '.img';
+
+        (new ImageWriter())->write($destination, 'https://commons.wikimedia.org/wiki/Special:FilePath/Cover.png');
+
+        self::assertFileExists($destination);
+        Http::assertSentCount(2);
+
+        File::delete($destination);
+    }
+
+    #[Test]
+    public function asksOnlyOnceWhenTheBrowserUserAgentIsServed(): void
+    {
+        $image = File::get(test_path('fixtures/cover.png'));
+
+        Http::fake(['*' => Http::response($image, 200, ['Content-Type' => 'image/png'])]);
+
+        $destination = sys_get_temp_dir() . '/' . Str::uuid() . '.img';
+
+        (new ImageWriter())->write($destination, 'https://example.com/cover.png');
+
+        self::assertFileExists($destination);
+        Http::assertSentCount(1);
 
         File::delete($destination);
     }
