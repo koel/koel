cce8a5e3491614ba10754a098b9d920b7f2a739f
Phan An <me@phanan.net>
2026-09-24 12:47:14 +0700

    feat: let TRUSTED_HOSTS allow every subdomain of a host (#2721)

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -3,18 +3,31 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustHosts as IlluminateTrustHost;
+use Illuminate\Support\Str;
 
 class TrustHosts extends IlluminateTrustHost
 {
+    private const string SUBDOMAIN_WILDCARD = '*.';
+
     /**
      * @return array<int, string>
      */
     public function hosts(): array
     {
         return collect(config('app.trusted_hosts'))
+            ->map(static fn (string $host): string => trim($host))
             ->filter()
-            ->map(static fn (string $host): string => '^' . preg_quote(trim($host), '{') . '$')
+            ->map(self::patternFor(...))
             ->values()
             ->all();
+    }
+
+    private static function patternFor(string $host): string
+    {
+        if (Str::startsWith($host, self::SUBDOMAIN_WILDCARD)) {
+            return '^.+\.' . preg_quote(Str::after($host, self::SUBDOMAIN_WILDCARD), '{') . '$';
+        }
+
+        return '^' . preg_quote($host, '{') . '$';
     }
 }
